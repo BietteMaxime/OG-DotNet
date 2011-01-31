5c37290e634eb5543388ee0259e7afe1471e902a
Simon Jackson <simon@opengamma.com>
2011-01-31 11:57:24 +0000

    Split another file

--- OGDotNet-Analytics/MainWindow.xaml.cs
+++ OGDotNet-Analytics/MainWindow.xaml.cs
@@ -16,7 +16,6 @@ using Fudge;
 using Fudge.Serialization;
 using Fudge.Types;
 using OGDotNet;
-using OGDotNet_Analytics.Mappedtypes.financial.model.interestrate.curve;
 using OGDotNet_Analytics.Mappedtypes.LiveData;
 using OGDotNet_Analytics.Mappedtypes.math.curve;
 using XamlReader = System.Windows.Markup.XamlReader;
@@ -63,61 +62,6 @@ namespace OGDotNet_Analytics
             {
 
                 new Thread(() => RefreshMyData(viewName, _cancellationTokenSource.Token)).Start();
-            }
-        }
-
-        public class PrimitiveCellTemplateSelector : DataTemplateSelector
-        {
-            private readonly string _column;
-
-            private static readonly Dictionary<Tuple<string,Type>, DataTemplate> TemplateCache = new Dictionary<Tuple<string, Type>, DataTemplate>();
-
-            public PrimitiveCellTemplateSelector(string column)
-            {
-                _column = column;
-            }
-
-            public override DataTemplate SelectTemplate(object item, DependencyObject container)
-            {//TODO reuse the templates
-
-                var column = _column;
-                var cellValue = ((PrimitiveRow)item)[column];
-                var type = cellValue == null ? typeof(Object) : cellValue.GetType();
-
-                var key = new Tuple<string, Type>(column, type);
-
-                DataTemplate ret;
-                if (! TemplateCache.TryGetValue(key, out ret))
-                {
-                    ret = BuildTemplate(column, type);
-                    TemplateCache[key] = ret;
-                }
-                return ret;
-            }
-
-            private static DataTemplate BuildTemplate(string column, Type type)
-            {
-                var binding = new Binding(string.Format(".[{0}]", column));//TODO bugs galore
-                
-
-                
-
-                if (typeof(YieldCurve).IsAssignableFrom(type) )
-                {
-                    var comboFactory = new FrameworkElementFactory(typeof(YieldCurveCell));
-                    comboFactory.SetValue(DataContextProperty, binding);  
-
-                    var itemsTemplate = new DataTemplate {VisualTree = comboFactory};
-
-                    return itemsTemplate;
-                }
-                else
-                {
-                    var comboFactory = new FrameworkElementFactory(typeof(TextBlock));
-                    comboFactory.SetValue(TextBlock.TextProperty, binding);
-                    var itemsTemplate = new DataTemplate {VisualTree = comboFactory};
-                    return itemsTemplate;
-                }
             }
         }
 
