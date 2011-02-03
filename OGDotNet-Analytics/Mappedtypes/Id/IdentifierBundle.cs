20021b921bc78e6a47992a485a8b11a3af4fd086
Simon Jackson <simon@opengamma.com>
2011-02-03 12:35:06 +0000

    Kill some comments

--- OGDotNet-Analytics/Mappedtypes/Id/IdentifierBundle.cs
+++ OGDotNet-Analytics/Mappedtypes/Id/IdentifierBundle.cs
@@ -5,12 +5,6 @@ using Fudge.Serialization;
 
 namespace OGDotNet_Analytics.Mappedtypes.Id
 {
-    /**
-* Provides uniform access to objects that can supply a unique identifier.
-* <p>
-* This interface makes no guarantees about the thread-safety of implementations.
-* However, wherever possible calls to this method should be thread-safe.
-*/
     public class IdentifierBundle
     {
         private readonly HashSet<Identifier> _identifiers;
--- OGDotNet-Analytics/Mappedtypes/Id/UniqueIdentifier.cs
+++ OGDotNet-Analytics/Mappedtypes/Id/UniqueIdentifier.cs
@@ -6,68 +6,30 @@ using OGDotNet_Analytics.Utils;
 
 namespace OGDotNet_Analytics.Mappedtypes.Id
 {
+    /// <summary>
+    /// TODO: .netify this and finish it
+    /// </summary>
     public class UniqueIdentifier : IComparable<UniqueIdentifier>, IEquatable<UniqueIdentifier>
     {
 
         public const String SCHEME_FUDGE_FIELD_NAME = "Scheme";
-        /**
-         * Fudge message key for the value.
-         */
         public const String VALUE_FUDGE_FIELD_NAME = "Value";
-        /**
-         * Fudge message key for the version.
-         */
         public const String VERSION_FUDGE_FIELD_NAME = "Version";
 
-        /**
-         * The scheme that categorizes the identifier value.
-         */
         private readonly String _scheme;
-        /**
-         * The identifier value within the scheme.
-         */
         private readonly String _value;
-        /**
-         * The version of the identifier, null if latest or not-versioned.
-         */
         private readonly String _version;
 
-        /**
-         * Obtains an identifier from a scheme and value indicating the latest version
-         * of the identifier, also used for non-versioned identifiers.
-         * 
-         * @param scheme  the scheme of the identifier, not empty, not null
-         * @param value  the value of the identifier, not empty, not null
-         * @return the identifier, not null
-         */
         public static UniqueIdentifier Of(String scheme, String value)
         {
             return Of(scheme, value, null);
         }
 
-        /**
-         * Obtains an identifier from a scheme, value and version.
-         * 
-         * @param scheme  the scheme of the identifier, not empty, not null
-         * @param value  the value of the identifier, not empty, not null
-         * @param version  the version of the identifier, empty treated as null, null treated as latest version
-         * @return the identifier, not null
-         */
         public static UniqueIdentifier Of(String scheme, String value, String version)
         {
             return new UniqueIdentifier(scheme, value, version);
         }
 
-        /**
-         * Obtains an identifier from a formatted scheme and value.
-         * <p>
-         * This parses the identifier from the form produced by {@code toString()}
-         * which is {@code <SCHEME>::<VALUE>::<VERSION>}.
-         * 
-         * @param uidStr  the identifier to parse, not null
-         * @return the identifier, not null
-         * @throws IllegalArgumentException if the identifier cannot be parsed
-         */
         public static UniqueIdentifier Parse(String uidStr)
         {
 
@@ -83,13 +45,6 @@ namespace OGDotNet_Analytics.Mappedtypes.Id
             throw new ArgumentException("Invalid identifier format: " + uidStr);
         }
 
-        /**
-         * Creates an instance.
-         * 
-         * @param scheme  the scheme of the identifier, not empty, not null
-         * @param value  the value of the identifier, not empty, not null
-         * @param version  the version of the identifier, null if latest version
-         */
         public UniqueIdentifier(String scheme, String value, String version)
         {
             ArgumentChecker.NotEmpty(scheme, "scheme");
@@ -100,60 +55,26 @@ namespace OGDotNet_Analytics.Mappedtypes.Id
         }
 
         //-------------------------------------------------------------------------
-        /**
-         * Gets the scheme of the identifier.
-         * <p>
-         * This is extracted from the object identifier.
-         * This is not expected to be the same as {@code IdentificationScheme}.
-         * 
-         * @return the scheme, not empty, not null
-         */
         public String Scheme
         {
             get { return _scheme; }
         }
 
-        /**
-         * Gets the value of the identifier.
-         * <p>
-         * This is extracted from the object identifier.
-         * 
-         * @return the value, not empty, not null
-         */
         public String Value
         {
             get { return _value; }
         }
 
-        /**
-         * Gets the version of the identifier.
-         * 
-         * @return the version, null if latest version
-         */
         public String Version
         {
             get { return _version; }
         }
 
-        /**
-         * Checks if this represents the latest version of the item.
-         * <p>
-         * This simply checks if the version is null.
-         * 
-         * @return true if this is the latest version
-         */
         public bool IsLatest
         {
             get { return _version == null; }
         }
 
-        /**
-         * Checks if this represents a versioned reference to the item.
-         * <p>
-         * This simply checks if the version is non null.
-         * 
-         * @return true if this is a versioned reference
-         */
         public bool IsVersioned
         {
             get
@@ -162,13 +83,6 @@ namespace OGDotNet_Analytics.Mappedtypes.Id
             }
         }
 
-        /**
-         * Returns a unique identifier based on this with the version set to null.
-         * <p>
-         * The returned identifier will represent the latest version of the item.
-         * 
-         * @return an identifier representing the latest version of the item, not null
-         */
         public UniqueIdentifier ToLatest()
         {
             if (IsVersioned)
