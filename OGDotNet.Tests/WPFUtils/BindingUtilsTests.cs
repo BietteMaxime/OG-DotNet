3d4540d6e44551b1dc397398840038d82e69a8d4
Simon Jackson <simon@opengamma.com>
2011-02-04 14:02:41 +0000

    rationalize libs and setup test project

---
+++ OGDotNet.Tests/WPFUtils/BindingUtilsTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+using NUnit.Framework;
+using OGDotNet.WPFUtils;
+
+namespace OGDotNet.Tests.WPFUtils
+{
+
+    [TestFixture]
+    public class BindingUtilsTests
+    {
+        [Test]
+        public void BindingModeIsOneWay()
+        {
+            var indexerBinding = BindingUtils.GetIndexerBinding("");
+            Assert.AreEqual(BindingMode.OneWay, indexerBinding.Mode);
+        }
+
+
+        [Test]                           
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("\t")]
+        [TestCase("Abba/FestFolk")]
+        [TestCase("Abba\\FestFolk")]
+        [TestCase("Abba/Fest Folk")]
+        [TestCase("Abba/[FestFolk]")]
+        [TestCase("Abba/[FestFolk")]
+        [TestCase("Abba/[FestFolk]")]
+        [TestCase("Abba/Fest%Folk")]
+        [TestCase("Abba/Fest^Folk")]
+        public void IndexValueTests(string indexer)
+        {
+            string boundValue = RoundTripBinding(indexer);
+            Assert.AreEqual(indexer, boundValue);
+        }
+
+        private static string RoundTripBinding(string indexer)
+        {
+            var indexerBinding = BindingUtils.GetIndexerBinding(indexer);
+
+            var boundClass = new BoundClass {DataContext = new ReflectingIndexer()};
+            boundClass.SetBinding(TextBlock.TextProperty, indexerBinding);
+
+
+            return boundClass.BoundValue;
+        }
+
+        public class ReflectingIndexer
+        {
+            public string this[String s]
+            {
+                get { return s; }
+            }
+        }
+        public class BoundClass : TextBlock
+        {
+            public string BoundValue
+            {
+                get
+                {
+                    return (string)GetValue(TextProperty);
+                }
+            }
+        }
+    }
+}
+
--- OGDotNet.WPFUtils/BindingUtils.cs
+++ OGDotNet.WPFUtils/BindingUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Windows.Data;
 
 namespace OGDotNet.WPFUtils
@@ -8,9 +9,15 @@ namespace OGDotNet.WPFUtils
         public static Binding GetIndexerBinding(string index)
         {
             //need to ^ escape some things http://msdn.microsoft.com/en-us/library/ms752300.aspx
-
-            var safeIndex = index.Replace("^","^^").Replace("[", "^[").Replace("]","^]");
-            return new Binding(String.Format(".[{0}]", safeIndex));
+            //I could attempt to guess, but escaping everything is way easier
+            
+            var stringBuilder = new StringBuilder(new string('^', index.Length*2));
+            
+            for (int i = 0; i < index.Length; i += 1)
+            {
+                stringBuilder[i*2 +1 ] = index[i];
+            }
+            return new Binding(String.Format(".[{0}]", stringBuilder)){Mode = BindingMode.OneWay};
         }
     }
 }
