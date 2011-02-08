fbd4a307e34a6e5d8228590cff202d6d72ee9925
Simon Jackson <simon@opengamma.com>
2011-02-08 12:21:40 +0000

    Add test to spot some TODOs

---
+++ OGDotNet.Tests.Integration/OGDotNet/Mappedtypes/EmptyTypesWarner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OGDotNet.Mappedtypes.Id;
+using OGDotNet.Tests.Integration.Xunit.Extensions;
+using Xunit;
+using Xunit.Extensions;
+
+namespace OGDotNet.Tests.Integration.OGDotNet.Mappedtypes
+{
+    public class EmptyTypesWarner
+    {
+        public static IEnumerable<Type> MappedTypes
+        {
+            get
+            {
+                var mappedTypes = typeof (UniqueIdentifier).Assembly.GetTypes().Where(t => t.FullName.StartsWith("OGDotNet.Mappedtypes")).ToList();
+                Assert.NotEmpty(mappedTypes);
+                return mappedTypes;
+            }
+        }
+
+        [Theory(Skip = "Known fault - [TODO add JIRA ID]")]
+        [TypedPropertyData("MappedTypes")]
+        public void TypesArentUseless(Type mappedType)
+        {
+            Assert.True(GetUsefuleness(mappedType)  > GetUsefuleness(typeof(object)), string.Format("Useless mapped type {0}", mappedType.FullName));
+        }
+
+        private static int GetUsefuleness(Type mappedType)
+        {
+            return mappedType.GetProperties().Count() + mappedType.GetMethods().Count();
+        }
+    }
+}
