aa055ab1cb841fb98cef75218b9ae2377f2c7f51
Simon Jackson <simon@opengamma.com>
2011-02-09 10:01:14 +0000

    Add a pause test

--- OGDotNet.Tests.Integration/OGDotNet/Resources/RemoteViewClientTests.cs
+++ OGDotNet.Tests.Integration/OGDotNet/Resources/RemoteViewClientTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using OGDotNet.Model.Resources;
 using OGDotNet.Tests.Integration.Xunit.Extensions;
 using Xunit;
@@ -23,19 +24,80 @@ namespace OGDotNet.Tests.Integration.OGDotNet.Resources
         [TypedPropertyData("Views")]
         public void CanStartAndGetAResult(RemoteView view)
         {
+            GetSomeResults(view,1);
+        }
+
+        [Theory]
+        [TypedPropertyData("Views")]
+        public void CanGetManyResults(RemoteView view)
+        {
+            GetSomeResults(view, 3);
+        }
+
+        private static void GetSomeResults(RemoteView view, int resultsToGet)
+        {
             view.Init();
             using (var remoteViewClient = view.CreateClient())
             {
                 var cts = new CancellationTokenSource();
                 var resultsEnum = remoteViewClient.GetResults(cts.Token);
+
+                int i = 0;
+                foreach (var viewComputationResultModel in resultsEnum)
+                {
+                    Assert.NotNull(viewComputationResultModel);
+                    
+                    i++;
+
+                    if (i >= resultsToGet)
+                        break;
+                }
+
+            }
+        }
+
+
+        [Theory]
+        [TypedPropertyData("Views")]
+        public void CanPauseAndRestart(RemoteView view)
+        {
+            const int forbiddenAfterPause = 3;
+
+            var timeout = TimeSpan.FromMilliseconds(10000 * forbiddenAfterPause);
+
+            view.Init();
+            using (var remoteViewClient = view.CreateClient())
+            {
+                var cts = new CancellationTokenSource();
+                var resultsEnum = remoteViewClient.GetResults(cts.Token);
+
                 using (var enumerator = resultsEnum.GetEnumerator())
                 {
                     enumerator.MoveNext();
                     Assert.NotNull(enumerator.Current);
+
+                    remoteViewClient.Pause();
+
+                    var mre = new ManualResetEvent(false);
+                    ThreadPool.QueueUserWorkItem(delegate
+                    {
+                        for (int i = 0; i < forbiddenAfterPause; i++)
+                        {
+                            enumerator.MoveNext();
+                            Assert.NotNull(enumerator.Current);
+                        }
+                        mre.Set();
+                    });
+
+                    bool gotResults = mre.WaitOne(timeout);
+                    Assert.False(gotResults, string.Format("I got {0} results for view {1} within {2} after pausing", forbiddenAfterPause, view.Name, timeout));
+
+                    remoteViewClient.Start();
+                    gotResults = mre.WaitOne(timeout);
+                    Assert.True(gotResults, string.Format("I didn't {0} results for view {1} within {2} after starting", forbiddenAfterPause, view.Name, timeout));
                 }
+
             }
         }
-
-        //TODO pause tests
     }
 }
