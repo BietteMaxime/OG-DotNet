7d7ea521fcc79975868b0da26598c29b2acae061
Simon Jackson <simon@opengamma.com>
2011-02-09 11:05:01 +0000

    Make some assertions about the view results

--- OGDotNet.Tests.Integration/OGDotNet/Resources/RemoteViewClientTests.cs
+++ OGDotNet.Tests.Integration/OGDotNet/Resources/RemoteViewClientTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
+using OGDotNet.Mappedtypes.Core.Position;
+using OGDotNet.Mappedtypes.engine.View;
 using OGDotNet.Model.Resources;
 using OGDotNet.Tests.Integration.Xunit.Extensions;
 using Xunit;
@@ -58,14 +61,9 @@ namespace OGDotNet.Tests.Integration.OGDotNet.Resources
 
 
         [Theory]
-        [TypedPropertyData("Views")]
+        [TypedPropertyData("FastTickingViews")]
         public void CanPauseAndRestart(RemoteView view)
         {
-            if (view.Name == "Primitives Only")
-                return;//Too slow updates to be useful
-            if (view.Name.StartsWith("Cash Equity"))
-                return;//Too slow updates to be useful
-
             const int forbiddenAfterPause = 3;
 
             var timeout = TimeSpan.FromMilliseconds(5000 * forbiddenAfterPause);
@@ -103,6 +101,93 @@ namespace OGDotNet.Tests.Integration.OGDotNet.Resources
                 }
 
             }
+        }
+
+
+
+        [Theory]
+        [TypedPropertyData("FastTickingViews")]
+        public void ViewResultsMatchDefinition(RemoteView view)
+        {
+            var countMaxExpectedValues = CountMaxExpectedValues(view);
+
+            view.Init();
+            using (var remoteViewClient = view.CreateClient())
+            {
+                var cts = new CancellationTokenSource();
+                var resultsEnum = remoteViewClient.GetResults(cts.Token);
+
+                int cycles = 1;
+
+                foreach (var viewComputationResultModel in resultsEnum)
+                {
+                    foreach (var viewResultEntry in viewComputationResultModel.AllResults)
+                    {
+                        AssertDefinitionContains(view, viewResultEntry);
+                    }
+
+                    
+                    var countActualValues = viewComputationResultModel.AllResults.Count();
+                        
+                    Console.Out.WriteLine("{0} {1} {2}",view.Name, countActualValues, countMaxExpectedValues);
+                    Assert.InRange(countActualValues, 1, countMaxExpectedValues);
+
+                    if (--cycles <=0) 
+                        return;
+                }
+
+            }
+        }
+
+        private static void AssertDefinitionContains(RemoteView view, ViewResultEntry viewResultEntry)
+        {
+            var configuration = view.Definition.CalculationConfigurationsByName[viewResultEntry.CalculationConfiguration];
+
+            var valueSpecification = viewResultEntry.ComputedValue.Specification;
+
+            foreach (var req in configuration.SpecificRequirements)
+            {
+                bool matches = (req.ComputationTargetIdentifier ==
+                                valueSpecification.TargetSpecification.Uid
+                                && req.ComputationTargetType == valueSpecification.TargetSpecification.Type
+                                && req.ValueName == valueSpecification.ValueName);
+                //TODO constraints?
+                if (matches)
+                    return;
+
+            }
+            
+            var reqsByType= configuration.PortfolioRequirementsBySecurityType;
+            foreach (var valuePropertiese in reqsByType)
+            {
+                var hashSet = valuePropertiese.Value.Properties["portfolioRequirement"];
+                if (!hashSet.Contains(valueSpecification.ValueName))
+                {
+                    Assert.True(false, string.Format("Unmatched requirement {0},{1},{2} on {3}", valueSpecification.ValueName,
+                                                     valueSpecification.TargetSpecification.Type,
+                                                     valueSpecification.TargetSpecification.Uid, view.Name));
+                }
+            }
+        }
+
+        private static int CountMaxExpectedValues(RemoteView view)
+        {
+            var specifics = view.Definition.CalculationConfigurationsByName.Sum(kvp => kvp.Value.SpecificRequirements.Count());
+            int rows = CountRows(view.Portfolio);
+            var values = rows * view.Definition.CalculationConfigurationsByName.Sum(kvp => kvp.Value.PortfolioRequirementsBySecurityType.Values.Sum(kvp2 => kvp2.Properties["portfolioRequirement"].Count));
+            return specifics
+                + values
+                ;
+        }
+
+        private static int CountRows(IPortfolio portfolio)
+        {
+            return CountRows(portfolio.Root);
+        }
+
+        private static int CountRows(PortfolioNode portfolio)
+        {
+            return (1 + portfolio.Positions.Count()) + portfolio.SubNodes.Sum(n => CountRows(n));
         }
     }
 }
--- OGDotNet.Tests.Integration/OGDotNet/Resources/ViewTestsBase.cs
+++ OGDotNet.Tests.Integration/OGDotNet/Resources/ViewTestsBase.cs
@@ -12,6 +12,17 @@ namespace OGDotNet.Tests.Integration.OGDotNet.Resources
                                                                       "TestDefinition"//Broken
                                                                   };
 
+        private static bool IsSlowTickingView(RemoteView view)
+        {
+            if (view.Name == "Primitives Only")
+                return true;
+            if (view.Name == "Bond Future Test View")
+                return true;
+            if (view.Name.StartsWith("Cash Equity"))
+                return true;
+            return false;
+        }
+
         public static IEnumerable<string> ViewNames
         {
             get
@@ -27,6 +38,15 @@ namespace OGDotNet.Tests.Integration.OGDotNet.Resources
             {
                 var remoteEngineContext = GetContext();
                 return ViewNames.Where(n => !BannedViews.Contains(n)).Select(n => remoteEngineContext.ViewProcessor.GetView(n));
+            }
+        }
+
+        public static IEnumerable<RemoteView> FastTickingViews
+        {
+            get
+            {
+                var remoteEngineContext = GetContext();
+                return ViewNames.Where(n => !BannedViews.Contains(n)).Select(n => remoteEngineContext.ViewProcessor.GetView(n)).Where(n => ! IsSlowTickingView(n));
             }
         }
     }
