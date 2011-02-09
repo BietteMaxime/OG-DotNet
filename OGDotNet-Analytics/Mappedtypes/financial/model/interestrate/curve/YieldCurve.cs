5b5897ba9f9bef7142859405b43338cfc4bbf808
Simon Jackson <simon@opengamma.com>
2011-02-09 18:06:37 +0000

    Add some handy convenience methods from java

--- OGDotNet-Analytics/Mappedtypes/financial/model/interestrate/curve/YieldCurve.cs
+++ OGDotNet-Analytics/Mappedtypes/financial/model/interestrate/curve/YieldCurve.cs
@@ -1,9 +1,21 @@
-﻿using OGDotNet.Mappedtypes.math.curve;
+﻿using System;
+using OGDotNet.Mappedtypes.math.curve;
 
 namespace OGDotNet.Mappedtypes.financial.model.interestrate.curve
 {
     public class YieldCurve
     {
         public Curve Curve { get; set; }
+
+
+        public double GetInterestRate(double t)
+        {
+            return Curve.GetYValue(t);
+        }
+
+        public double GetDiscountFactor(double t)
+        {
+            return Math.Exp(-t * GetInterestRate(t));
+        }
     }
 }
