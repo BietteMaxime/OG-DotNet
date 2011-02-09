0506841c15d014db7e580c547c6c1d9236b08bc4
Simon Jackson <simon@opengamma.com>
2011-02-09 16:29:59 +0000

    Immutable ftw

--- OGDotNet-Analytics/Mappedtypes/Core/Position/PortfolioNode.cs
+++ OGDotNet-Analytics/Mappedtypes/Core/Position/PortfolioNode.cs
@@ -4,9 +4,37 @@ namespace OGDotNet.Mappedtypes.Core.Position
 {
     public class PortfolioNode
     {
-        public string Identifier { get; set; }
-        public string Name { get; set; }
-        public IList<PortfolioNode> SubNodes { get; set; }
-        public IList<Position> Positions { get; set; }
+        private readonly string _identifier;
+        private readonly string _name;
+        private readonly IList<PortfolioNode> _subNodes;
+        private readonly IList<Position> _positions;
+
+        public PortfolioNode(string identifier, string name, IList<PortfolioNode> subNodes, IList<Position> positions)
+        {
+            _identifier = identifier;
+            _name = name;
+            _subNodes = subNodes;
+            _positions = positions;
+        }
+
+        public string Identifier
+        {
+            get { return _identifier; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public IList<PortfolioNode> SubNodes
+        {
+            get { return _subNodes; }
+        }
+
+        public IList<Position> Positions
+        {
+            get { return _positions; }
+        }
     }
 }
