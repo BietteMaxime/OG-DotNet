6de561d52743d02658b088c29601bb4b8acde14d
Simon Jackson <simon@opengamma.com>
2011-02-09 16:43:17 +0000

    Type up ResultOutputMode

--- OGDotNet-Analytics/Mappedtypes/engine/View/ResultModelDefinition.xaml.cs
+++ OGDotNet-Analytics/Mappedtypes/engine/View/ResultModelDefinition.xaml.cs
@@ -1,12 +1,66 @@
-﻿namespace OGDotNet.Mappedtypes.engine.View
+﻿using System;
+using Fudge;
+using Fudge.Serialization;
+using OGDotNet.Mappedtypes.engine.depGraph.DependencyGraph;
+
+namespace OGDotNet.Mappedtypes.engine.View
 {
     public class ResultModelDefinition
     {
-        public string AggregatePositionOutputMode { get; set; }
-        public string PositionOutputMode { get; set; }
-        public string TradeOutputMode { get; set; }
-        public string SecurityOutputMode { get; set; }
-        public string PrimitiveOutputMode { get; set; }
-        //TODO shouldOutputResult
+        private readonly ResultOutputMode _aggregatePositionOutputMode;
+        private readonly ResultOutputMode _positionOutputMode;
+        private readonly ResultOutputMode _tradeOutputMode;
+        private readonly ResultOutputMode _securityOutputMode;
+        private readonly ResultOutputMode _primitiveOutputMode;
+
+        public ResultModelDefinition(ResultOutputMode aggregatePositionOutputMode, ResultOutputMode positionOutputMode, ResultOutputMode tradeOutputMode,  ResultOutputMode securityOutputMode, ResultOutputMode primitiveOutputMode)
+        {
+            _aggregatePositionOutputMode = aggregatePositionOutputMode;
+            _positionOutputMode = positionOutputMode;
+            _tradeOutputMode = tradeOutputMode;
+            _securityOutputMode = securityOutputMode;
+            _primitiveOutputMode = primitiveOutputMode;
+        }
+
+        public ResultOutputMode AggregatePositionOutputMode
+        {
+            get { return _aggregatePositionOutputMode; }
+        }
+
+        public ResultOutputMode PositionOutputMode
+        {
+            get { return _positionOutputMode; }
+        }
+
+        public ResultOutputMode TradeOutputMode
+        {
+            get { return _tradeOutputMode; }
+        }
+
+        public ResultOutputMode SecurityOutputMode
+        {
+            get { return _securityOutputMode; }
+        }
+
+        public ResultOutputMode PrimitiveOutputMode
+        {
+            get { return _primitiveOutputMode; }
+        }
+
+        public static ResultModelDefinition FromFudgeMsg(IFudgeFieldContainer ffc, IFudgeDeserializer deserializer)
+        {
+            return new ResultModelDefinition(
+                ResultOutputModeMethods.Parse(ffc.GetValue<string>("aggregatePositionOutputMode")),
+                ResultOutputModeMethods.Parse(ffc.GetValue<string>("positionOutputMode")),
+                ResultOutputModeMethods.Parse(ffc.GetValue<string>("tradeOutputMode")),
+                ResultOutputModeMethods.Parse(ffc.GetValue<string>("securityOutputMode")),
+                ResultOutputModeMethods.Parse(ffc.GetValue<string>("primitiveOutputMode"))
+                );
+        }
+
+        public void ToFudgeMsg(IAppendingFudgeFieldContainer a, IFudgeSerializer s)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ OGDotNet-Analytics/Mappedtypes/engine/depGraph/DependencyGraph/ResultOutputMode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OGDotNet.Mappedtypes.engine.depGraph.DependencyGraph
+{
+    public enum ResultOutputMode
+    {
+        //TODO shouldOutputResult
+        None,
+        TerminalOutputs,
+        All
+    }
+
+    public static class ResultOutputModeMethods
+    {
+
+        internal static ResultOutputMode Parse(string getValue)
+        {
+            ResultOutputMode ret;
+            if (! Enum.TryParse(getValue.Replace("_",""), true, out ret))
+            {
+                throw new ArgumentException();
+            }
+            return ret;
+        }
+    }
+}
