6a9d0da7031362c95824077e5cfb0c513c420a4c
Simon Jackson <simon@opengamma.com>
2011-02-09 17:42:33 +0000

    Add new curve type to the model, and match the type hierarchy

---
+++ OGDotNet-Analytics/Mappedtypes/math/curve/Curve.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fudge;
+
+namespace OGDotNet.Mappedtypes.math.curve
+{
+    public static class CurveExtensionMethods
+    {
+        public static IEnumerable<Tuple<double,double>> GetData(this Curve c)
+        {
+            return c.XData.Zip(c.YData, (x, y) => new Tuple<double, double>(x, y)).ToList();
+        }
+    }
+    public abstract class Curve
+    {
+        private readonly string _name;
+
+        protected Curve(string name)
+        {
+            _name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public abstract IList<double> XData { get; }
+        public abstract IList<double> YData { get; }
+
+        public abstract double GetYValue(double x);
+
+        protected static string GetName(IFudgeFieldContainer ffc)
+        {
+            return ffc.GetValue<string>("curve name");
+        }
+    }
+}
--- OGDotNet-Analytics/Mappedtypes/math/curve/FunctionalDoublesCurve.cs
+++ OGDotNet-Analytics/Mappedtypes/math/curve/FunctionalDoublesCurve.cs
@@ -2,11 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Fudge;
+using Fudge.Serialization;
+using OGDotNet.Mappedtypes.financial.model.interestrate;
 
 namespace OGDotNet.Mappedtypes.math.curve
 {
     //TODO
-    public class FunctionalDoublesCurve
+    public class FunctionalDoublesCurve : Curve
     {
+        private readonly Func<double, double> _function;
+
+        private FunctionalDoublesCurve(Func<double, double> function, string name) : base(name)
+        {
+            _function = function;
+        }
+
+        public static FunctionalDoublesCurve FromFudgeMsg(IFudgeFieldContainer ffc, IFudgeDeserializer deserializer)
+        {
+            string name = GetName(ffc);
+            double[] parameters = ffc.GetValue<double[]>("NSS parameters");
+            return From(new NelsonSiegelSvennsonBondCurveModel(parameters).Eval, name);
+        }
+
+        public override IList<double> XData
+        {
+            get
+            {
+                throw new InvalidOperationException("Cannot get x data - this curve is defined by a function (x -> y)");
+            }
+        }
+
+        public override IList<double> YData
+        {
+            get
+            {
+                throw new InvalidOperationException("Cannot get y data - this curve is defined by a function (x -> y)");
+            }
+        }
+
+
+        public override double GetYValue(double x)
+        {
+            return _function(x);
+        }
+
+        public static FunctionalDoublesCurve From(Func<double,double> function, string name)
+        {
+            return new FunctionalDoublesCurve(function, name);
+        }
+
+        public void ToFudgeMsg(IAppendingFudgeFieldContainer a, IFudgeSerializer s)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
