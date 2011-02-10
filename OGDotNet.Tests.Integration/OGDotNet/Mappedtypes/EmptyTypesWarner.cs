e82a5deb528725b9b77d98e7c084603c0bc66736
Simon Jackson <simon@opengamma.com>
2011-02-10 09:34:57 +0000

    This test should now pass

--- OGDotNet.Tests.Integration/OGDotNet/Mappedtypes/EmptyTypesWarner.cs
+++ OGDotNet.Tests.Integration/OGDotNet/Mappedtypes/EmptyTypesWarner.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using OGDotNet.Mappedtypes.Id;
 using OGDotNet.Tests.Integration.Xunit.Extensions;
 using Xunit;
@@ -20,16 +21,22 @@ namespace OGDotNet.Tests.Integration.OGDotNet.Mappedtypes
             }
         }
 
-        [Theory(Skip = "Known fault - [TODO add JIRA ID]")]
+        private static readonly int LeastUseful = typeof(object).GetProperties().Count() + typeof(object).GetMethods().Count();
+
+        [Theory]
         [TypedPropertyData("MappedTypes")]
         public void TypesArentUseless(Type mappedType)
         {
-            Assert.True(GetUsefuleness(mappedType)  > GetUsefuleness(typeof(object)), string.Format("Useless mapped type {0}", mappedType.FullName));
+            if (mappedType.IsInterface)
+                return;
+            Assert.True(GetUsefuleness(mappedType)  > LeastUseful, string.Format("Useless mapped type {0}", mappedType.FullName));
         }
 
         private static int GetUsefuleness(Type mappedType)
         {
-            return mappedType.GetProperties().Count() + mappedType.GetMethods().Count();
+            BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | (mappedType == typeof(object) ? 0 : BindingFlags.Static);
+            
+            return mappedType.GetProperties(bindingFlags).Count() + mappedType.GetMethods(bindingFlags).Count();
         }
     }
 }
