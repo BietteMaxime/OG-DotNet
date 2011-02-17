7484e8417325e5daf76671b86e7ba1bf7a94424b
Simon Jackson <simon@opengamma.com>
2011-02-17 13:37:46 +0000

    Don't do timeouts when debugging

--- OGDotNet.Tests.Integration/Xunit.Extensions/ManualTimeout.cs
+++ OGDotNet.Tests.Integration/Xunit.Extensions/ManualTimeout.cs
@@ -10,7 +10,7 @@ namespace OGDotNet.Tests.Integration.Xunit.Extensions
     {
         private static int DefaultTimeout
         {
-            get { return (int) TimeSpan.FromMinutes(5).TotalMilliseconds; }
+            get { return Debugger.IsAttached ?  int.MaxValue : (int) TimeSpan.FromMinutes(5).TotalMilliseconds; }
         }
 
         internal static T ExecuteWithTimeout<T>(Func<T> work)
