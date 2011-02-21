e28f22f027d5bca59fd06dd3fc10527ef757a8ea
Simon Jackson <simon@opengamma.com>
2011-02-21 16:49:31 +0000

    Zoom

--- OGDotNet-SecurityViewer/View/SecurityTimeSeriesWindow.xaml.cs
+++ OGDotNet-SecurityViewer/View/SecurityTimeSeriesWindow.xaml.cs
@@ -2,6 +2,9 @@
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.DataVisualization.Charting;
+using System.Windows.Input;
 using System.Windows.Threading;
 using OGDotNet.Mappedtypes.Core.Security;
 using OGDotNet.Model.Context;
@@ -15,7 +18,7 @@ namespace OGDotNet.SecurityViewer.View
     public partial class SecurityTimeSeriesWindow : Window
     {
         private RemoteHistoricalDataSource _dataSource;
-
+        
         public SecurityTimeSeriesWindow()
         {
             InitializeComponent();
@@ -75,5 +78,190 @@ namespace OGDotNet.SecurityViewer.View
         {
             Dispatcher.BeginInvoke(act, DispatcherPriority.ContextIdle);
         }
+
+        #region zooming
+
+
+        private Point startZoomPosition;
+
+        private void chart_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            startZoomPosition = e.GetPosition(canvas);
+            zoomRectangle.Visibility=Visibility.Visible;
+
+            chart_MouseMove(sender,e);
+        }
+
+        private void chart_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton!=MouseButtonState.Pressed)
+                zoomRectangle.Visibility = Visibility.Hidden;
+            if (e.RightButton==MouseButtonState.Pressed)
+                zoomRectangle.Visibility = Visibility.Hidden;
+
+            if (zoomRectangle.Visibility != Visibility.Visible)
+                return;
+
+            var endZoomPosition = e.GetPosition(canvas);
+
+            Canvas.SetLeft(zoomRectangle, Math.Min(endZoomPosition.X,startZoomPosition.X));
+            Canvas.SetTop(zoomRectangle, Math.Min(endZoomPosition.Y, startZoomPosition.Y));
+            zoomRectangle.Width = Math.Abs(endZoomPosition.X - startZoomPosition.X);
+            zoomRectangle.Height= Math.Abs(endZoomPosition.Y - startZoomPosition.Y);
+        }
+
+        private void chart_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (zoomRectangle.Visibility != Visibility.Visible)
+                return;
+
+            var endZoomPosition = e.GetPosition(canvas);
+            
+            SetXRange(startZoomPosition, endZoomPosition);
+            SetYRange(startZoomPosition, endZoomPosition);
+           
+
+            zoomRectangle.Visibility = Visibility.Hidden;
+        }
+
+        private void chart_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            zoomRectangle.Visibility = Visibility.Hidden;
+        }
+
+        private void chart_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            zoomRectangle.Visibility = Visibility.Hidden;
+
+            YAxis.Minimum = null;
+            YAxis.Maximum = null;
+
+            XAxis.Minimum = null;
+            XAxis.Maximum = null;
+        }
+
+        
+
+        private void chart_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Released || e.RightButton != MouseButtonState.Released)
+                return;
+            if ((Keyboard.Modifiers & ~ModifierKeys.Control) != 0)
+                return;
+
+            bool moving = (Keyboard.Modifiers & ModifierKeys.Control) == 0;
+
+
+            var centrePoint = e.GetPosition(canvas);
+            var centre = GetChartPosition(centrePoint);
+
+            if (moving)
+            {
+                double mult = e.Delta / 601.0;
+
+                var range = YAxis.ActualMaximum.Value - YAxis.ActualMinimum.Value;
+                YAxis.Minimum = YAxis.ActualMinimum.Value + range * mult;
+                YAxis.Maximum = YAxis.ActualMaximum.Value + range * mult;
+            }
+            else
+            {
+                double mult = e.Delta/361.0;
+                XAxis.Minimum = XAxis.ActualMinimum.Value + Mult(centre.Item1 - XAxis.ActualMinimum.Value, mult);
+                XAxis.Maximum = XAxis.ActualMaximum.Value - Mult(XAxis.ActualMaximum.Value - centre.Item1, mult);
+
+                YAxis.Minimum = YAxis.ActualMinimum.Value + (centre.Item2 - YAxis.ActualMinimum.Value)*mult;
+                YAxis.Maximum = YAxis.ActualMaximum.Value - (YAxis.ActualMaximum.Value - centre.Item2)*mult;
+            }
+        }
+
+        private void SetXRange(Point startPosition, Point endPosition)
+        {
+            var x1 = GetChartPosition(startPosition).Item1;
+            var x2 = GetChartPosition(endPosition).Item1;
+
+            if (x1 == x2)
+            {
+                XAxis.Minimum = x1;
+                XAxis.Maximum = x1;
+            }
+            else
+            {
+                XAxis.Minimum = Min(x1, x2);
+                XAxis.Maximum = Max(x1, x2);
+            }
+        }
+
+        private void SetYRange(Point startPosition, Point endPosition)
+        {
+            var y1 = GetChartPosition(startPosition).Item2;
+            var y2 = GetChartPosition(endPosition).Item2;
+
+            if (y1==y2)
+            {
+                YAxis.Minimum = y1;
+                YAxis.Maximum = y1;
+            }
+            else
+            {
+                YAxis.Minimum = Math.Min(y1, y2);
+                YAxis.Maximum= Math.Max(y1, y2);
+            }
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="p">relative to <see cref="canvas"/></param>
+        /// <returns></returns>
+        private Tuple<DateTime,double> GetChartPosition(Point p)
+        {
+            var yAxisY = canvas.TranslatePoint(p, YAxis).Y;
+            var yMax = YAxis.ActualMaximum.Value;
+            var yMin = YAxis.ActualMinimum.Value;
+            var yRange = yMax - yMin;
+            var yScale = yRange * 1.0 / YAxis.ActualHeight;
+
+            var yScaled = yMax - (yScale * yAxisY);
+
+            var xAxisX = canvas.TranslatePoint(p, XAxis).X;
+            var xMax = XAxis.ActualMaximum.Value;
+            var xMin = XAxis.ActualMinimum.Value;
+            var xRange = xMax - xMin;
+            var xScale = Mult(xRange, 1.0 / XAxis.ActualWidth);
+
+            var xScaled = xMin + (Mult(xScale,xAxisX));
+
+            return Tuple.Create(xScaled, yScaled);
+        }
+
+        private DateTimeAxis XAxis
+        {
+            get { return (DateTimeAxis)lineSeries.ActualIndependentAxis; }
+        }
+
+        private NumericAxis YAxis
+        {
+            get { return (NumericAxis)lineSeries.ActualDependentRangeAxis; }
+        }
+
+        private static TimeSpan Mult(TimeSpan timeSpan, double factor)
+        {
+            return TimeSpan.FromMilliseconds(timeSpan.TotalMilliseconds * factor);
+        }
+        private static DateTime Max(DateTime x1, DateTime x2)
+        {
+            return x1 > x2 ? x1 : x2;
+        }
+
+        private static DateTime Min(DateTime x1, DateTime x2)
+        {
+            return x1 > x2 ? x2 : x1;
+        }
+        
+
+        #endregion
+
+        
+
+        
     }
 }
