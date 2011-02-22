47fd4a1e2734ebeb8312f93a1aa3ccb5e7f16a54
Simon Jackson <simon@opengamma.com>
2011-02-22 12:49:43 +0000

    Add CurrencyMatrixSourcingFunction

---
+++ OGDotNet-Analytics/Mappedtypes/financial/currency/CurrencyMatrixSourcingFunction.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using OGDotNet.Mappedtypes.engine.View;
+using Currency = OGDotNet.Mappedtypes.Core.Common.Currency;
+
+namespace OGDotNet.Mappedtypes.financial.currency
+{
+    public class CurrencyMatrixSourcingFunction
+    {
+        private readonly CurrencyMatrix _matrix;
+
+        public CurrencyMatrixSourcingFunction(CurrencyMatrix matrix)
+        {
+            _matrix = matrix;
+        }
+
+        /// <summary>
+        /// TODO type up first input
+        /// </summary>
+        public double GetConversionRate( Func<ValueRequirement,object> inputs, Currency source, Currency target)
+        {
+            var currencyMatrixValue = _matrix.GetConversion(source,target);
+            if (currencyMatrixValue is CurrencyMatrixValue.CurrencyMatrixCross)
+            {
+                var cross = (CurrencyMatrixValue.CurrencyMatrixCross)currencyMatrixValue ;
+                return GetConversionRate(inputs, source, cross.CrossCurrency)*GetConversionRate(inputs, cross.CrossCurrency, target);
+            }
+            else if (currencyMatrixValue is CurrencyMatrixValue.CurrencyMatrixFixed)
+            {
+                var fixedValue = (CurrencyMatrixValue.CurrencyMatrixFixed)currencyMatrixValue;
+                return fixedValue.FixedValue;
+            }
+            else if (currencyMatrixValue is CurrencyMatrixValue.CurrencyMatrixValueRequirement)
+            {
+                var valueRequirement = (CurrencyMatrixValue.CurrencyMatrixValueRequirement)currencyMatrixValue;
+
+                double rate = (double)inputs(valueRequirement.ValueRequirement);
+                if (valueRequirement.Reciprocal)
+                {
+                    rate = 1.0 / rate;
+                }
+                return rate;
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ OGDotNet.Tests.Integration/OGDotNet/Resources/CurrencyMatrixSourcingFunctionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using OGDotNet.Mappedtypes.Core.Common;
+using OGDotNet.Mappedtypes.engine.View;
+using OGDotNet.Mappedtypes.financial.currency;
+using Xunit;
+
+namespace OGDotNet.Tests.Integration.OGDotNet.Resources
+{
+    public class CurrencyMatrixSourcingFunctionTests : TestWithContextBase
+    {
+        [Fact]
+        public void CanGetIdentity()
+        {
+            CurrencyMatrixSourcingFunction currencyMatrixSourcingFunction = GetFunction();
+            var conversionRate = currencyMatrixSourcingFunction.GetConversionRate(GetValue, Currency.Create("USD"), Currency.Create("USD"));
+            Assert.Equal(1.0,conversionRate);
+        }
+        
+
+        [Fact]
+        public void CanGetNonIdentity()
+        {
+            CurrencyMatrixSourcingFunction currencyMatrixSourcingFunction = GetFunction();
+            var conversionRate = currencyMatrixSourcingFunction.GetConversionRate(GetValue, Currency.Create("GBP"), Currency.Create("USD"));
+            Assert.NotEqual(1.0, conversionRate);
+        }
+
+        private CurrencyMatrixSourcingFunction GetFunction()
+        {
+            var currencyMatrix = Context.CurrencyMatrixSource.GetCurrencyMatrix("BloombergLiveData");
+            return new CurrencyMatrixSourcingFunction(currencyMatrix);
+        }
+
+        private static object GetValue(ValueRequirement arg)
+        {
+            return 2.0;
+        }
+    }
+}
