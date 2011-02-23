6aa05a19c2e6a99f2e482c9c0571573957cae84f
Simon Jackson <simon@opengamma.com>
2011-02-23 11:40:52 +0000

    Make OpenGammaFudgeContext fast and thread safe

--- OGDotNet-Analytics/Model/OpenGammaFudgeContext.cs
+++ OGDotNet-Analytics/Model/OpenGammaFudgeContext.cs
@@ -1,35 +1,60 @@
-﻿using Castle.Core;
+﻿using System;
+using System.Diagnostics;
+using Castle.Core;
 using Fudge;
 using Fudge.Serialization;
+using Fudge.Serialization.Reflection;
+using OGDotNet.Utils;
 
 namespace OGDotNet.Model
 {
     [Singleton]
     public class OpenGammaFudgeContext : FudgeContext
     {
-        private SerializationTypeMap _typeMap;//This is kept around becuase of its very useful cache
+        private readonly MemoizingFudgeSurrogateSelector _fudgeSurrogateSelector;
 
         public  OpenGammaFudgeContext()
         {
             SetProperty(ContextProperties.TypeMappingStrategyProperty, new JavaTypeMappingStrategy("OGDotNet.Mappedtypes", "com.opengamma"));
             SetProperty(ContextProperties.FieldNameConventionProperty, FudgeFieldNameConvention.CamelCase);
-            _typeMap = new SerializationTypeMap(this);
+            _fudgeSurrogateSelector = new MemoizingFudgeSurrogateSelector(this);
         }
-
 
         public FudgeSerializer GetSerializer()
         {
-            return GetSerializer(this);
+            //We can't cache at the SerializationTypeMap level
+            // a. It's not thread safe
+            // b. We shouldn't artificially inflate the type indexes
+            // see 3c356af3736508f1c2a18146a28af6dca4bf8525
+            return new FudgeSerializer(this, new SerializationTypeMap(this, _fudgeSurrogateSelector));
         }
 
 
-        private FudgeSerializer GetSerializer(FudgeContext context)
+        /// <remarks>
+        /// NOTE: There's all sorts of bugs with this if the context changes.
+        ///         We're largely saved by the fact that we only cache Surrogates we found, not the failure to find one
+        /// </remarks>
+        private class MemoizingFudgeSurrogateSelector  : FudgeSurrogateSelector
         {
-            if (_typeMap == null)
+            readonly Memoizer<Type,FudgeFieldNameConvention,IFudgeSerializationSurrogate> _memoizer;
+
+            public MemoizingFudgeSurrogateSelector(FudgeContext context) : base(context)
             {
-                _typeMap = new SerializationTypeMap(context);
+                _memoizer = new Memoizer<Type, FudgeFieldNameConvention, IFudgeSerializationSurrogate>(GetSurrogateImpl);
             }
-            return new FudgeSerializer(context, _typeMap);
+
+            public override IFudgeSerializationSurrogate GetSurrogate(Type type, FudgeFieldNameConvention fieldNameConvention)
+            {
+                var fudgeSerializationSurrogate = _memoizer.Get(type, fieldNameConvention);
+                Debug.Assert(fudgeSerializationSurrogate!=null, "Returning a null surrogate may well expose our bugs");
+                return fudgeSerializationSurrogate;
+            }
+
+            private IFudgeSerializationSurrogate GetSurrogateImpl(Type type, FudgeFieldNameConvention fieldNameConvention)
+            {
+                return base.GetSurrogate(type, fieldNameConvention);
+            }
+
         }
     }
 }
---
+++ OGDotNet.Tests.Integration/OGDotNet/Model/OpenGammaFudgeContextTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading.Tasks;
+using Fudge.Serialization;
+using OGDotNet.Model;
+using Xunit;
+using A = OGDotNet.Mappedtypes.OpenGammaFudgeContextTestsTypes.A;
+using B = OGDotNet.Mappedtypes.OpenGammaFudgeContextTestsTypes.B;
+using C = OGDotNet.Mappedtypes.OpenGammaFudgeContextTestsTypes.C;
+using FactAttribute = OGDotNet.Tests.Integration.Xunit.Extensions.FactAttribute;
+namespace OGDotNet.Tests.Integration.OGDotNet.Model
+{
+    public class OpenGammaFudgeContextTests
+    {
+        [Fact]
+        public void SerializerKindOfWorks()
+        {
+            var context = new OpenGammaFudgeContext();
+            Thrash(context);
+        }
+
+        [Fact]
+        public void SerializerIsThreadSafe()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                var context = new OpenGammaFudgeContext();
+                var parallelLoopResult = Parallel.For(1, 4 * Environment.ProcessorCount, _ => Thrash(context));
+            }
+        }
+
+        /// <summary>
+        /// This tries to check that the cacheing which breaks <see cref="SerializerIsThreadSafe"/> actually works
+        /// </summary>
+        [Fact(Timeout = 12000)]
+        public void SerializerIsFast()
+        {
+            var context = new OpenGammaFudgeContext();
+            
+            for (int i = 0; i < 100000; i++)
+            {
+                Thrash(context);
+            }
+        }
+
+        private static FudgeSerializer Thrash(OpenGammaFudgeContext context)
+        {
+            var fudgeSerializer = context.GetSerializer();
+            var graph = new A { B = new B { C = new C { A = new A { B = new B() } } } };
+            var msg = fudgeSerializer.SerializeToMsg(graph);
+            A graphRet = fudgeSerializer.Deserialize<A>(msg);
+            Assert.NotNull(graphRet.B.C.A.B);
+            Assert.Null(graphRet.B.C.A.B.C);
+            return fudgeSerializer;
+        }
+
+        
+    }
+}
+namespace OGDotNet.Mappedtypes
+{
+    public static class OpenGammaFudgeContextTestsTypes
+    {
+        [Serializable]
+        public class A
+        {
+            public B B;
+        }
+        [Serializable]
+        public class B
+        {
+            public C C;
+        }
+        [Serializable]
+        public class C
+        {
+            public A A;
+        }
+    }
+}
