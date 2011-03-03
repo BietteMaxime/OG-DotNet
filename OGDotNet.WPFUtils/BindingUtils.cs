f515b233b13cde056d209078c3a3b286f4f9a890
Simon Jackson <simon@opengamma.com>
2011-03-03 16:38:22 +0000

    Kill the stupid implementation of BindingUtils

--- OGDotNet.WPFUtils/BindingUtils.cs
+++ OGDotNet.WPFUtils/BindingUtils.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Text;
+﻿using System.Windows;
 using System.Windows.Data;
 
 namespace OGDotNet.WPFUtils
@@ -8,16 +7,7 @@ namespace OGDotNet.WPFUtils
     {
         public static Binding GetIndexerBinding(string index)
         {
-            //need to ^ escape some things http://msdn.microsoft.com/en-us/library/ms752300.aspx
-            //I could attempt to guess, but escaping everything is way easier
-            
-            var stringBuilder = new StringBuilder(new string('^', index.Length*2));
-            
-            for (int i = 0; i < index.Length; i += 1)
-            {
-                stringBuilder[i*2 +1 ] = index[i];
-            }
-            return new Binding(String.Format(".[{0}]", stringBuilder)){Mode = BindingMode.OneWay};
+            return new Binding{Mode = BindingMode.OneWay, Path =  new PropertyPath(".[(0)]", index)};
         }
     }
 }
