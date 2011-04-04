8419a74b7ba1e58408c3ada2692a341f8f1eeaf4
Simon Jackson <simon@opengamma.com>
2011-04-04 17:24:32 +0100

    Implement some volatility surfaces

--- OGDotNet-Analytics/Mappedtypes/financial/model/volatility/surface/VolatilitySurface.cs
+++ OGDotNet-Analytics/Mappedtypes/financial/model/volatility/surface/VolatilitySurface.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Fudge;
+using Fudge.Serialization;
+using OGDotNet.Mappedtypes.math.surface;
 
 namespace OGDotNet.Mappedtypes.financial.model.volatility.surface
 {
@@ -10,5 +13,18 @@ namespace OGDotNet.Mappedtypes.financial.model.volatility.surface
     /// </summary>
     public class VolatilitySurface
     {
+        private readonly ConstantDoublesSurface _sigma;
+
+        public VolatilitySurface(ConstantDoublesSurface sigma)
+        {
+            _sigma = sigma;
+        }
+
+        public ConstantDoublesSurface Sigma
+        {
+            get { return _sigma; }
+        }
     }
+
+
 }
---
+++ OGDotNet-Analytics/Mappedtypes/math/surface/ConstantDoublesSurface.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Fudge;
+using Fudge.Serialization;
+
+namespace OGDotNet.Mappedtypes.math.surface
+{
+    public class ConstantDoublesSurface
+    {
+         private readonly double _z;
+        private readonly string _name;
+
+        private ConstantDoublesSurface(double z, string name)
+        {
+            _z = z;
+            _name = name;
+        }
+
+        public double Z
+        {
+            get { return _z; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public static ConstantDoublesSurface FromFudgeMsg(IFudgeFieldContainer ffc, IFudgeDeserializer deserializer)
+        {
+            double z = ffc.GetDouble("z value").Value;
+            string name = ffc.GetString("surface name");
+            return new ConstantDoublesSurface(z,name);
+        }
+
+        public void ToFudgeMsg(IAppendingFudgeFieldContainer a, IFudgeSerializer s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
