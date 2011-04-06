73b94f2d5547d86b76727558d831ff67107e17ca
Simon Jackson <simon@opengamma.com>
2011-04-06 12:33:36 +0100

    Add a basic test to catch thinkos like the one I just made

---
+++ OGDotNet.Tests/OGDotNet/Mappedtypes/Core/marketdatasnapshot/MarketDataValueTypeTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OGDotNet.Builders;
+using OGDotNet.Mappedtypes.Core.marketdatasnapshot;
+using OGDotNet.Mappedtypes.engine;
+using OGDotNet.Tests.Xunit.Extensions;
+using Xunit;
+using Xunit.Extensions;
+
+namespace OGDotNet.Tests.OGDotNet.Mappedtypes.Core.marketdatasnapshot
+{
+    public class MarketDataValueTypeTests
+    {
+        [Theory]
+        [EnumValuesData]
+        public void CanRoundTrip(MarketDataValueType type)
+        {
+            var computationTargetType = EnumUtils<MarketDataValueType,ComputationTargetType>.ConvertTo(type);
+            var roundTripped = EnumUtils<ComputationTargetType, MarketDataValueType>.ConvertTo(computationTargetType);
+            Assert.Equal(type,roundTripped);
+        }
+    }
+}
---
+++ OGDotNet.Tests/Xunit.Extensions/EnumValuesDataAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xunit.Extensions;
+
+namespace OGDotNet.Tests.Xunit.Extensions
+{
+    class EnumValuesDataAttribute : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest, Type[] parameterTypes)
+        {
+            var type = parameterTypes.Single();
+            if (! type.IsEnum)
+            {
+                throw new ArgumentException();
+            }
+
+            return from object value in Enum.GetValues(type) select new[]{value};
+        }
+    }
+}
