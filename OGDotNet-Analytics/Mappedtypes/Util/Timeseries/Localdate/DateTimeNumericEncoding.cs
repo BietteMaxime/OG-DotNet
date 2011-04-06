b12d8d8f48f2e80de879e918c6cedc48a40a0c1e
Simon Jackson <simon@opengamma.com>
2011-04-06 13:01:36 +0100

    Undo a nonsensical commit (reverse-merge from commit 7ae262e2f619d68b84b4bbedfea2e2ff9f0f801b)

--- OGDotNet-Analytics/Mappedtypes/Util/Timeseries/Localdate/DateTimeNumericEncoding.cs
+++ OGDotNet-Analytics/Mappedtypes/Util/Timeseries/Localdate/DateTimeNumericEncoding.cs
@@ -40,7 +40,7 @@ namespace OGDotNet.Mappedtypes.util.timeseries.fast
         
         public DateTime ConvertToDateTime(long sourceValue)
         {
-            var daysSinceEpoch = ConvertToLong(sourceValue, _encoding);
+            var daysSinceEpoch = ConvertToLong(sourceValue, Encoding.DATE_EPOCH_DAYS);//TODO don't use days
             return Epoch + TimeSpan.FromDays(daysSinceEpoch);
         }
 
