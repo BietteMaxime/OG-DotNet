87975e4002fdb8550388dd2c3caca94ba215f6f7
Simon Jackson <simon@opengamma.com>
2011-04-14 14:45:16 +0100

    DOTNET-13 : one more StyleCop rule FileMayOnlyContainASingleNamespace

--- OGDotNet.Tests.Integration/OGDotNet/Model/OpenGammaFudgeContextTests.cs
+++ OGDotNet.Tests.Integration/OGDotNet/Model/OpenGammaFudgeContextTests.cs
@@ -62,24 +62,3 @@ namespace OGDotNet.Tests.Integration.OGDotNet.Model
         }
     }
 }
-namespace OGDotNet.Mappedtypes
-{
-    public static class OpenGammaFudgeContextTestsTypes
-    {
-        [Serializable]
-        public class A
-        {
-            public B B;
-        }
-        [Serializable]
-        public class B
-        {
-            public C C;
-        }
-        [Serializable]
-        public class C
-        {
-            public A A;
-        }
-    }
-}
