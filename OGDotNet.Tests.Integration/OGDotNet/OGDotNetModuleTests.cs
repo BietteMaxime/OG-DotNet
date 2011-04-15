ad129e484a3d9a6a5d5b66fe1c0240f9079cafcd
Simon Jackson <simon@opengamma.com>
2011-04-15 11:03:43 +0100

    Test the castle module

---
+++ OGDotNet.Tests.Integration/OGDotNet/OGDotNetModuleTests.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OGDotNetModuleTests.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
+//   Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+//
+//     Please see distribution for license.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using OGDotNet.Model;
+using OGDotNet.Model.Context;
+using Xunit;
+
+namespace OGDotNet.Tests.Integration.OGDotNet
+{
+    public class OGDotNetModuleTests
+    {
+        [Xunit.Extensions.Fact]
+        public void CanLoadModuleExplicit()
+        {
+            var container = new WindsorContainer();
+
+            var defaultConfigurationStore = new DefaultConfigurationStore();
+            var ogDotNetModule = new OGDotNetModule();
+            ogDotNetModule.Install(container, defaultConfigurationStore);
+            
+            AssertResolvable(container);
+        }
+
+        [Xunit.Extensions.Fact]
+        public void CanLoadModuleImplicit()
+        {
+            var container = new WindsorContainer();
+
+            var defaultConfigurationStore = new DefaultConfigurationStore();
+            FromAssembly.Containing<RemoteEngineContextFactory>().Install(container, defaultConfigurationStore);
+
+            AssertResolvable(container);
+        }
+
+        private static void AssertResolvable(WindsorContainer container)
+        {
+            var openGammaFudgeContext = container.Resolve<OpenGammaFudgeContext>();
+            Assert.NotNull(openGammaFudgeContext);
+
+            Assert.Throws<ComponentRegistrationException>(() => container.Register(Component.For<RemoteEngineContext>().UsingFactoryMethod<RemoteEngineContext>(() => null)));
+        }
+    }
+}
