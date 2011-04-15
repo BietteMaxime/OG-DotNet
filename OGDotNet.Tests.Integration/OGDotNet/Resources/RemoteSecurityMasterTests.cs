a81cd04d809fb996092ee2a186e2f403beab3dde
Simon Jackson <simon@opengamma.com>
2011-04-15 11:45:06 +0100

    Add test to demonstrate DOTNET-16

---
+++ OGDotNet.Tests.Integration/OGDotNet/Resources/RemoteSecurityMasterTests.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RemoteSecurityMasterTests.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
+//   Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+//
+//     Please see distribution for license.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Linq;
+using OGDotNet.Mappedtypes.Id;
+using OGDotNet.Mappedtypes.Util.Db;
+using Xunit;
+
+namespace OGDotNet.Tests.Integration.OGDotNet.Resources
+{
+    public class RemoteSecurityMasterTests : TestWithContextBase
+    {
+        [Xunit.Extensions.Fact(Skip = "DOTNET-16")]
+        public void CanSearchWithRequest()
+        {
+            var searchResult = Context.SecurityMaster.Search("*", "FUTURE", PagingRequest.All, new IdentifierSearch(Enumerable.Empty<Identifier>(), IdentifierSearchType.Any));
+            Assert.NotEmpty(searchResult.Documents);
+
+            var securitytoFind = searchResult.Documents.First();
+            var identifierBundle = securitytoFind.Security.Identifiers;
+
+            var singleSearchResult = Context.SecurityMaster.Search("*", "FUTURE", PagingRequest.All, new IdentifierSearch(identifierBundle.Identifiers, IdentifierSearchType.Any));
+            Assert.NotEmpty(singleSearchResult.Documents);
+            Assert.Single(singleSearchResult.Documents);
+            Assert.Equal(singleSearchResult.Documents.Single().Security.UniqueId, UniqueIdentifier.Parse(securitytoFind.UniqueId));
+        }
+    }
+}
