14104c395d161458713a7afd288d8959038ff867
Simon Jackson <simon@opengamma.com>
2011-05-03 14:44:36 +0100

    Unicode comments

--- OGDotNet-AnalyticsViewer/View/CellTemplates/YieldCurveCell.xaml.cs
+++ OGDotNet-AnalyticsViewer/View/CellTemplates/YieldCurveCell.xaml.cs
@@ -1,4 +1,4 @@
-//-----------------------------------------------------------------------
+﻿//-----------------------------------------------------------------------
 // <copyright file="YieldCurveCell.xaml.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
 //     Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
 //
