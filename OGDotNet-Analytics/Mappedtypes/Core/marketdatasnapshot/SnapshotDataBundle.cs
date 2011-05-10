c5c1c515ed74c64107cee6124d915287f17e4bdf
Simon Jackson <simon@opengamma.com>
2011-05-10 11:24:35 +0100

    Handle the new data bundles

---
+++ OGDotNet-Analytics/Mappedtypes/Core/marketdatasnapshot/SnapshotDataBundle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Fudge;
+using Fudge.Serialization;
+using OGDotNet.Builders;
+using OGDotNet.Mappedtypes.Id;
+using OGDotNet.Utils;
+
+namespace OGDotNet.Mappedtypes.Core.marketdatasnapshot
+{
+    public class SnapshotDataBundle
+    {
+        private readonly Dictionary<Identifier, double> _dataPoints;
+
+        public SnapshotDataBundle(Dictionary<Identifier, double>   dataPoints)
+        {
+            ArgumentChecker.NotNull(dataPoints, "dataPoints");
+            _dataPoints = dataPoints;
+        }
+
+        public Dictionary<Identifier, double> DataPoints
+        {
+            get { return _dataPoints; }
+        }
+
+        public static SnapshotDataBundle FromFudgeMsg(IFudgeFieldContainer ffc, IFudgeDeserializer deserializer)
+        {
+            var map = MapBuilder.FromFudgeMsg(ffc.GetMessage("dataPoints"), f=>Identifier.Parse((string) f.Value), f=>(double) f.Value);
+            return new SnapshotDataBundle(map);
+        }
+
+        public void ToFudgeMsg(IAppendingFudgeFieldContainer a, IFudgeSerializer s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
