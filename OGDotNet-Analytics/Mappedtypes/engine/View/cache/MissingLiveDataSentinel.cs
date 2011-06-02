a48d522ce03b6f3bd8fec4838d1b76fd1bd1b23d
Simon Jackson <simon@opengamma.com>
2011-06-02 13:59:23 +0100

    Add MissingLiveDataSentinel

---
+++ OGDotNet-Analytics/Mappedtypes/engine/View/cache/MissingLiveDataSentinel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fudge;
+using Fudge.Serialization;
+using OGDotNet.Builders;
+
+namespace OGDotNet.Mappedtypes.engine.View.cache
+{
+    public class MissingLiveDataSentinel
+    {
+        public static readonly MissingLiveDataSentinel Instance = new MissingLiveDataSentinel();
+
+        private MissingLiveDataSentinel()
+        {
+        }
+
+        public static MissingLiveDataSentinel FromFudgeMsg(IFudgeFieldContainer ffc, IFudgeDeserializer deserializer)
+        {
+            return Instance;
+        }
+
+        public void ToFudgeMsg(IAppendingFudgeFieldContainer a, IFudgeSerializer s)
+        {
+            s.WriteTypeHeader(a, typeof (MissingLiveDataSentinel));
+        }
+    }
+}
