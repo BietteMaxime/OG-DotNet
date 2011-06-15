2b758193531427b0758f6168243b9edd6a31c36c
Simon Jackson <simon@opengamma.com>
2011-06-15 17:08:45 +0100

    equality checking

--- OGDotNet-Analytics/Mappedtypes/engine/View/NumberDeltaComparer.cs
+++ OGDotNet-Analytics/Mappedtypes/engine/View/NumberDeltaComparer.cs
@@ -11,7 +11,7 @@ using Fudge.Serialization;
 
 namespace OGDotNet.Mappedtypes.engine.view
 {
-    public class NumberDeltaComparer : IDeltaComparer<double>
+    public class NumberDeltaComparer : IDeltaComparer<double>, IEquatable<IDeltaComparer<double>>
     {
         private readonly int _decimalPlaces;
 
@@ -33,6 +33,31 @@ namespace OGDotNet.Mappedtypes.engine.view
         public void ToFudgeMsg(IAppendingFudgeFieldContainer a, IFudgeSerializer s)
         {
             a.Add("decimalPlaces", _decimalPlaces);
+        }
+
+        public bool Equals(IDeltaComparer<double> other)
+        {
+            return other is NumberDeltaComparer && Equals((NumberDeltaComparer) other);
+        }
+
+        public bool Equals(NumberDeltaComparer other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return other._decimalPlaces == _decimalPlaces;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof (NumberDeltaComparer)) return false;
+            return Equals((NumberDeltaComparer) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _decimalPlaces;
         }
     }
 }
