da207689bec8368f8614ee8876f6d1a0f05f6601
Simon Jackson <simon@opengamma.com>
2011-06-27 19:40:23 +0100

    Separate builder for DependencyNode

---
+++ OGDotNet-Analytics/Builders/DependencyNodeBuilder.cs
@@ -0,0 +1,90 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DependencyNodeBuilder.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
+//     Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+//
+//     Please see distribution for license.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using Fudge;
+using Fudge.Serialization;
+using OGDotNet.Mappedtypes.engine;
+using OGDotNet.Mappedtypes.engine.depgraph;
+using OGDotNet.Mappedtypes.engine.function;
+using OGDotNet.Mappedtypes.engine.value;
+
+namespace OGDotNet.Builders
+{
+    class DependencyNodeBuilder : BuilderBase<DependencyNode>
+    {
+        public DependencyNodeBuilder(FudgeContext context, Type type) : base(context, type)
+        {
+        }
+
+        public override DependencyNode DeserializeImpl(IFudgeFieldContainer ffc, IFudgeDeserializer deserializer)
+        {
+            var target = deserializer.FromField<ComputationTarget>(ffc.GetByName("target"));
+
+            string parameterizedFunctionUniqueId = ffc.GetString("parameterizedFunctionUniqueId");
+            var functionParametersField = ffc.GetByName("functionParameters");
+            var functionParameters = functionParametersField != null ? deserializer.FromField<IFunctionParameters>(functionParametersField) : null;
+
+            string functionShortName = ffc.GetString("functionShortName");
+            string functionUniqueId = ffc.GetString("functionUniqueId");
+
+            ICompiledFunctionDefinition function = new CompiledFunctionDefinitionStub(target.Type, functionShortName, functionUniqueId);
+            var parameterizedFunction = new ParameterizedFunction(function, functionParameters, parameterizedFunctionUniqueId);
+
+            var inputValues = deserializer.FromField<ValueSpecification[]>(ffc.GetByName("inputValues"));
+            var outputValues = deserializer.FromField<ValueSpecification[]>(ffc.GetByName("outputValues"));
+            var terminalOutputValues = deserializer.FromField<ValueSpecification[]>(ffc.GetByName("terminalOutputValues"));
+
+            return new DependencyNode(target, inputValues, outputValues, terminalOutputValues, parameterizedFunction);
+        }
+
+        private class CompiledFunctionDefinitionStub : ICompiledFunctionDefinition
+        {
+            private readonly ComputationTargetType _targetType;
+            private readonly FunctionDefinitionStub _functionDefinition;
+
+            public CompiledFunctionDefinitionStub(ComputationTargetType targetType, string uniqueId, string shortName)
+            {
+                _targetType = targetType;
+                _functionDefinition = new FunctionDefinitionStub(uniqueId, shortName);
+            }
+
+            public ComputationTargetType TargetType
+            {
+                get { return _targetType; }
+            }
+
+            public IFunctionDefinition FunctionDefinition
+            {
+                get { return _functionDefinition; }
+            }
+        }
+
+        private class FunctionDefinitionStub : IFunctionDefinition
+        {
+            private readonly string _uniqueId;
+            private readonly string _shortName;
+
+            public FunctionDefinitionStub(string uniqueId, string shortName)
+            {
+                _uniqueId = uniqueId;
+                _shortName = shortName;
+            }
+
+            public string UniqueId
+            {
+                get { return _uniqueId; }
+            }
+
+            public string ShortName
+            {
+                get { return _shortName; }
+            }
+        }
+    }
+}
--- OGDotNet-Analytics/Mappedtypes/engine/depGraph/DependencyNode.cs
+++ OGDotNet-Analytics/Mappedtypes/engine/depGraph/DependencyNode.cs
@@ -9,11 +9,13 @@ using System;
 using System.Collections.Generic;
 using Fudge;
 using Fudge.Serialization;
+using OGDotNet.Builders;
 using OGDotNet.Mappedtypes.engine.function;
 using OGDotNet.Mappedtypes.engine.value;
 
 namespace OGDotNet.Mappedtypes.engine.depgraph
 {
+    [FudgeSurrogate(typeof(DependencyNodeBuilder))]
     public class DependencyNode
     {
         private readonly IEnumerable<ValueSpecification> _inputValues;
@@ -23,7 +25,7 @@ namespace OGDotNet.Mappedtypes.engine.depgraph
         private readonly ComputationTarget _target;
         private readonly ICollection<DependencyNode> _inputNodes = new List<DependencyNode>();
 
-        private DependencyNode(ComputationTarget target, IEnumerable<ValueSpecification> inputValues, IEnumerable<ValueSpecification> outputValues, IEnumerable<ValueSpecification> terminalOutputValues, ParameterizedFunction function)
+        public DependencyNode(ComputationTarget target, IEnumerable<ValueSpecification> inputValues, IEnumerable<ValueSpecification> outputValues, IEnumerable<ValueSpecification> terminalOutputValues, ParameterizedFunction function)
         {
             _target = target;
             _inputValues = inputValues;
@@ -65,76 +67,6 @@ namespace OGDotNet.Mappedtypes.engine.depgraph
         public void AddInputNode(DependencyNode inputNode)
         {
             _inputNodes .Add(inputNode);
-        }
-
-        public static DependencyNode FromFudgeMsg(IFudgeFieldContainer ffc, IFudgeDeserializer deserializer)
-        {
-            var target = deserializer.FromField<ComputationTarget>(ffc.GetByName("target"));
-
-            string parameterizedFunctionUniqueId = ffc.GetString("parameterizedFunctionUniqueId");
-            var functionParametersField = ffc.GetByName("functionParameters");
-            var functionParameters = functionParametersField != null ? deserializer.FromField<IFunctionParameters>(functionParametersField) : null;
-
-            string functionShortName = ffc.GetString("functionShortName");
-            string functionUniqueId = ffc.GetString("functionUniqueId");
-            
-            ICompiledFunctionDefinition function = new CompiledFunctionDefinitionStub(target.Type, functionShortName, functionUniqueId);
-            var parameterizedFunction = new ParameterizedFunction(function, functionParameters, parameterizedFunctionUniqueId);
-
-            var inputValues = deserializer.FromField<ValueSpecification[]>(ffc.GetByName("inputValues"));
-            var outputValues = deserializer.FromField<ValueSpecification[]>(ffc.GetByName("outputValues"));
-            var terminalOutputValues = deserializer.FromField<ValueSpecification[]>(ffc.GetByName("terminalOutputValues"));
-
-            return new DependencyNode(target, inputValues, outputValues, terminalOutputValues, parameterizedFunction);
-        }
-
-        public void ToFudgeMsg(IAppendingFudgeFieldContainer a, IFudgeSerializer s)
-        {
-            throw new NotImplementedException();
-        }
-
-        private class CompiledFunctionDefinitionStub : ICompiledFunctionDefinition
-        {
-            private readonly ComputationTargetType _targetType;
-            private readonly FunctionDefinitionStub _functionDefinition;
-
-            public CompiledFunctionDefinitionStub(ComputationTargetType targetType, string uniqueId, string shortName)
-            {
-                _targetType = targetType;
-                _functionDefinition = new FunctionDefinitionStub(uniqueId, shortName);
-            }
-
-            public ComputationTargetType TargetType
-            {
-                get { return _targetType; }
-            }
-
-            public IFunctionDefinition FunctionDefinition
-            {
-                get { return _functionDefinition; }
-            }
-        }
-
-        private class FunctionDefinitionStub : IFunctionDefinition
-        {
-            private readonly string _uniqueId;
-            private readonly string _shortName;
-
-            public FunctionDefinitionStub(string uniqueId, string shortName)
-            {
-                _uniqueId = uniqueId;
-                _shortName = shortName;
-            }
-
-            public string UniqueId
-            {
-                get { return _uniqueId; }
-            }
-
-            public string ShortName
-            {
-                get { return _shortName; }
-            }
         }
     }
 }
