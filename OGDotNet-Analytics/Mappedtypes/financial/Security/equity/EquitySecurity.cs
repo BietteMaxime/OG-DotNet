f699dce0f458b85a935cf9dd2480c44a892aed78
Simon Jackson <simon@opengamma.com>
2011-06-27 20:07:54 +0100

    A couple of null securitie impls, that should be proto This stops a bit of reflection

---
+++ OGDotNet-Analytics/Mappedtypes/financial/Security/equity/EquitySecurity.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EquitySecurity.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
+//     Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+//
+//     Please see distribution for license.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using OGDotNet.Mappedtypes.Id;
+
+namespace OGDotNet.Mappedtypes.financial.Security.equity
+{
+    /// <summary>
+    /// TODO: kill these with the .proto replacement
+    /// </summary>
+    public class EquitySecurity : FinancialSecurity
+    {
+        public EquitySecurity(string name, string securityType, UniqueIdentifier uniqueId, IdentifierBundle identifiers) : base(name, securityType, uniqueId, identifiers)
+        {
+        }
+    }
+}
---
+++ OGDotNet-Analytics/Mappedtypes/financial/Security/option/EquityOptionSecurity.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EquityOptionSecurity.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
+//     Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+//
+//     Please see distribution for license.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OGDotNet.Mappedtypes.Id;
+
+namespace OGDotNet.Mappedtypes.financial.Security.option
+{
+    /// <summary>
+    /// TODO: kill these with the .proto replacement
+    /// </summary>
+    class EquityOptionSecurity : FinancialSecurity
+    {
+        public EquityOptionSecurity(string name, string securityType, UniqueIdentifier uniqueId, IdentifierBundle identifiers) : base(name, securityType, uniqueId, identifiers)
+        {
+        }
+    }
+}
