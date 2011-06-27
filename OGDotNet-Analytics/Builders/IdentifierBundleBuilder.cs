5b3918ac75d129a21da0b68c23bee178f6bb526c
Simon Jackson <simon@opengamma.com>
2011-06-27 20:14:25 +0100

    split builder for IdentifierBundle

---
+++ OGDotNet-Analytics/Builders/IdentifierBundleBuilder.cs
@@ -0,0 +1,50 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IdentifierBundleBuilder.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
+//     Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+//
+//     Please see distribution for license.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using Fudge;
+using Fudge.Serialization;
+using OGDotNet.Mappedtypes.Id;
+
+namespace OGDotNet.Builders
+{
+    class IdentifierBundleBuilder : BuilderBase<IdentifierBundle>
+    {
+        public IdentifierBundleBuilder(FudgeContext context, Type type) : base(context, type)
+        {
+        }
+
+        public override IdentifierBundle DeserializeImpl(IFudgeFieldContainer msg, IFudgeDeserializer deserializer)
+        {
+            var identifiers = new HashSet<Identifier>();
+
+            foreach (var field in msg.GetAllFields())
+            {
+                switch (field.Name)
+                {
+                    case "ID":
+                        var i = (Identifier)deserializer.FromField(field, typeof(Identifier));
+                        identifiers.Add(i);
+                        break;
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+            return new IdentifierBundle(identifiers);
+        }
+
+        protected override void SerializeImpl(IdentifierBundle obj, IAppendingFudgeFieldContainer msg, IFudgeSerializer serializer)
+        {
+            foreach (var identifier in obj.Identifiers)
+            {
+                msg.Add("ID", identifier);
+            }
+        }
+    }
+}
--- OGDotNet-Analytics/Mappedtypes/Id/IdentifierBundle.cs
+++ OGDotNet-Analytics/Mappedtypes/Id/IdentifierBundle.cs
@@ -6,15 +6,14 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using Fudge;
 using Fudge.Serialization;
+using OGDotNet.Builders;
 
 namespace OGDotNet.Mappedtypes.Id
 {
+    [FudgeSurrogate(typeof(IdentifierBundleBuilder))]
     public class IdentifierBundle
     {
         private readonly HashSet<Identifier> _identifiers;
@@ -30,33 +29,6 @@ namespace OGDotNet.Mappedtypes.Id
         public IEnumerable<Identifier> Identifiers
         {
             get { return _identifiers; }
-        }
-
-        public static IdentifierBundle FromFudgeMsg(IFudgeFieldContainer ffc, IFudgeDeserializer deserializer)
-        {
-            var identifiers = new HashSet<Identifier>();
-
-            foreach (var field in ffc.GetAllFields())
-            {
-                switch (field.Name)
-                {
-                    case "ID":
-                        var i = (Identifier)deserializer.FromField(field, typeof(Identifier));
-                        identifiers.Add(i);
-                        break;
-                    default:
-                        throw new ArgumentException();
-                }
-            }
-            return new IdentifierBundle(identifiers);
-        }
-
-        public void ToFudgeMsg(IAppendingFudgeFieldContainer a, IFudgeSerializer s)
-        {
-            foreach (var identifier in _identifiers)
-            {
-                a.Add("ID", identifier);
-            }
         }
 
         public override string ToString()
