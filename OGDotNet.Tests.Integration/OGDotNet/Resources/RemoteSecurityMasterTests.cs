4c1672287d5dc308835c9a085c82355d4b43e04c
Simon Jackson <simon@opengamma.com>
2011-06-29 12:29:39 +0100

    FX Options are back

--- OGDotNet.Tests.Integration/OGDotNet/Resources/RemoteSecurityMasterTests.cs
+++ OGDotNet.Tests.Integration/OGDotNet/Resources/RemoteSecurityMasterTests.cs
@@ -64,9 +64,6 @@ namespace OGDotNet.Tests.Integration.OGDotNet.Resources
                 var searchResult = Context.SecurityMaster.Search("*", securityType, PagingRequest.One);
                 switch (securityType)
                 {
-                    case "FX_OPTION":
-                        Assert.Equal(0, searchResult.Paging.TotalItems);
-                        break;
                     default:
                         Assert.NotEqual(0, searchResult.Paging.TotalItems);
                         Assert.Equal(securityType, searchResult.Documents.Single().Security.SecurityType);
