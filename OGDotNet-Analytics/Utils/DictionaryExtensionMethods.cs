2a2434ff2ad04f277952cd3ed75015c8f9d237f3
Simon Jackson <simon@opengamma.com>
2011-07-04 14:26:54 +0100

    Remove dead code

--- OGDotNet-Analytics/Utils/DictionaryExtensionMethods.cs
+++ OGDotNet-Analytics/Utils/DictionaryExtensionMethods.cs
@@ -14,34 +14,6 @@ namespace OGDotNet.Utils
 {
     static class DictionaryExtensionMethods
     {
-        internal static IDictionary<TKey, TValue> Merge<TKey, TValue>(this IDictionary<TKey, TValue> dicta, IDictionary<TKey, TValue> dictb)
-        {
-            return Merge(dicta, dictb, (a, b) => b);
-        }
-
-        internal static IDictionary<TKey, TValue> Merge<TKey, TValue>(this IDictionary<TKey, TValue> dicta, IDictionary<TKey, TValue> dictb, Func<TValue, TValue, TValue> merge)
-        {
-            var ret = new Dictionary<TKey, TValue>();
-
-            foreach (var key in dicta.Keys.Concat(dictb.Keys).Distinct())
-            {
-                TValue aValue;
-                var haveAValue = dicta.TryGetValue(key, out aValue);
-                TValue bValue;
-                var haveBValue = dictb.TryGetValue(key, out bValue);
-
-                if (haveAValue && haveBValue)
-                    ret[key] = merge(aValue, bValue);
-                else if (haveAValue)
-                    ret[key] = aValue;
-                else if (haveBValue)
-                    ret[key] = bValue;
-                else
-                    throw new ArgumentException();
-            }
-            return ret;
-        }
-
         public static IEnumerable<TRet> ProjectStructure<TKey, TValue, TRet>(this Dictionary<TKey, TValue> dictA, Dictionary<TKey, TValue> dictB,
             Func<TKey, TValue, TKey, TValue, TRet> matchingProjecter,
             Func<TKey, TValue, TRet> aOnlyProjecter,
