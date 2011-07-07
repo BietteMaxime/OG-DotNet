80f0a0349f0cc81e2b136a99e0aae4d56b7db573
Simon Jackson <simon@opengamma.com>
2011-07-07 12:23:03 +0100

    Split into separate file

--- OGDotNet-Analytics/Mappedtypes/financial/analytics/LabelledMatrixEntry.cs
+++ OGDotNet-Analytics/Mappedtypes/financial/analytics/LabelledMatrixEntry.cs
@@ -6,45 +6,10 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-using System;
 using OGDotNet.Utils;
 
 namespace OGDotNet.Mappedtypes.financial.analytics
 {
-    public class LabelledMatrixEntry2D
-    {
-        private readonly object _xLabel;
-        private readonly object _yLabel;
-        private readonly double _value;
-
-        public LabelledMatrixEntry2D(object xLabel, object yLabel, double value)
-        {
-            _xLabel = xLabel;
-            _yLabel = yLabel;
-            _value = value;
-        }
-
-        public object XLabel
-        {
-            get { return _xLabel; }
-        }
-
-        public object YLabel
-        {
-            get { return _yLabel; }
-        }
-
-        public double Value
-        {
-            get { return _value; }
-        }
-
-        public static explicit operator LabelledMatrixEntry(LabelledMatrixEntry2D f)
-        {
-            return new LabelledMatrixEntry(Tuple.Create(f.XLabel, f.YLabel), f.Value);
-        }
-    }
-
     public class LabelledMatrixEntry
     {
         private readonly object _label;
---
+++ OGDotNet-Analytics/Mappedtypes/financial/analytics/LabelledMatrixEntry2D.cs
@@ -0,0 +1,45 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="LabelledMatrixEntry2D.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
+//     Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+//
+//     Please see distribution for license.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
+
+namespace OGDotNet.Mappedtypes.financial.analytics
+{
+    public class LabelledMatrixEntry2D
+    {
+        private readonly object _xLabel;
+        private readonly object _yLabel;
+        private readonly double _value;
+
+        public LabelledMatrixEntry2D(object xLabel, object yLabel, double value)
+        {
+            _xLabel = xLabel;
+            _yLabel = yLabel;
+            _value = value;
+        }
+
+        public object XLabel
+        {
+            get { return _xLabel; }
+        }
+
+        public object YLabel
+        {
+            get { return _yLabel; }
+        }
+
+        public double Value
+        {
+            get { return _value; }
+        }
+
+        public static explicit operator LabelledMatrixEntry(LabelledMatrixEntry2D f)
+        {
+            return new LabelledMatrixEntry(Tuple.Create(f.XLabel, f.YLabel), f.Value);
+        }
+    }
+}
