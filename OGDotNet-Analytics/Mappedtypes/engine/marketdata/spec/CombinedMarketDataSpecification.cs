d6b1b58dd24a14a10994173d0b159ba30a1e4425
Simon Jackson <simon@opengamma.com>
2011-07-13 14:47:59 +0100

    PLAT-1401: CombinedMarketDataSpecification

---
+++ OGDotNet-Analytics/Mappedtypes/engine/marketdata/spec/CombinedMarketDataSpecification.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CombinedMarketDataSpecification.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
+//     Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+//
+//     Please see distribution for license.
+// </copyright>
+//-----------------------------------------------------------------------
+using Fudge.Serialization;
+using OGDotNet.Builders;
+
+namespace OGDotNet.Mappedtypes.engine.marketdata.spec
+{
+    [FudgeSurrogate(typeof(CombinedMarketDataSpecificationBuilder))]
+    public class CombinedMarketDataSpecification : MarketDataSpecification
+    {
+        private readonly MarketDataSpecification _prefferedSpecification;
+        private readonly MarketDataSpecification _fallbackSpecification;
+
+        public CombinedMarketDataSpecification(MarketDataSpecification prefferedSpecification, MarketDataSpecification fallbackSpecification)
+        {
+            _prefferedSpecification = prefferedSpecification;
+            _fallbackSpecification = fallbackSpecification;
+        }
+
+        public MarketDataSpecification PrefferedSpecification
+        {
+            get { return _prefferedSpecification; }
+        }
+
+        public MarketDataSpecification FallbackSpecification
+        {
+            get { return _fallbackSpecification; }
+        }
+    }
+}
