b7302ce28a5365fa5bd19c4687acafdb99298e0d
Simon Jackson <simon@opengamma.com>
2011-07-14 18:07:15 +0100

    Add broken PresentvalueSensitivity deserializer

--- OGDotNet-Analytics/Mappedtypes/financial/interestrate/PresentValueSensitivity.cs
+++ OGDotNet-Analytics/Mappedtypes/financial/interestrate/PresentValueSensitivity.cs
@@ -6,9 +6,54 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fudge;
+using Fudge.Serialization;
+
 namespace OGDotNet.Mappedtypes.financial.interestrate
 {
     public class PresentValueSensitivity
     {
+        private readonly Dictionary<string, List<Tuple<double, double>>> _sensitivities;
+
+        private PresentValueSensitivity(Dictionary<string, List<Tuple<double, double>>> sensitivities)
+        {
+            _sensitivities = sensitivities;
+        }
+
+        public Dictionary<string, List<Tuple<double, double>>> Sensitivities
+        {
+            get { return _sensitivities; }
+        }
+
+        public static PresentValueSensitivity FromFudgeMsg(IFudgeFieldContainer ffc, IFudgeDeserializer deserializer)
+        {
+            var data = new Dictionary<string, List<Tuple<double, double>>>();
+            var curveNames = ffc.GetAllByName("curve name");
+            var sensitivities = ffc.GetAllByName("sensitivities");
+            int id = 0;
+            foreach (var tuple in curveNames.Zip(sensitivities, Tuple.Create))
+            {
+                string curveName = "TODO " + (id++); //I believe the java is broken
+                var listSensitivities = (IFudgeFieldContainer) tuple.Item2;
+                var pairsFields = listSensitivities.GetAllByName(null);
+                var tuples = pairsFields.Select(ReadPair).ToList();
+                data.Add(curveName, tuples);
+            }
+            return new PresentValueSensitivity(data);
+        }
+
+        private static Tuple<double, double> ReadPair(IFudgeField f)
+        {
+            var fudgeFieldContainer = (IFudgeFieldContainer) f.Value;
+            return Tuple.Create(fudgeFieldContainer.GetDouble(0).Value, fudgeFieldContainer.GetDouble(1).Value);
+        }
+
+        public void ToFudgeMsg(IAppendingFudgeFieldContainer a, IFudgeSerializer s)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
