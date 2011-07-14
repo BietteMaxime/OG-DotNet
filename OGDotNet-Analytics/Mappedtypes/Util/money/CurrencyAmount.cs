f540ce060758e3bea4442c3784126d488d439363
Simon Jackson <simon@opengamma.com>
2011-07-14 13:03:13 +0100

    CurrencyAmount

---
+++ OGDotNet-Analytics/Mappedtypes/Util/money/CurrencyAmount.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CurrencyAmount.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
+//     Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+//
+//     Please see distribution for license.
+// </copyright>
+//-----------------------------------------------------------------------
+using OGDotNet.Mappedtypes.Core.Common;
+
+namespace OGDotNet.Mappedtypes.Util.money
+{
+    public class CurrencyAmount
+    {
+        private readonly Currency _currency;
+        private readonly double _amount;
+
+        public CurrencyAmount(Currency currency, double amount)
+        {
+            _currency = currency;
+            _amount = amount;
+        }
+
+        public Currency Currency
+        {
+            get { return _currency; }
+        }
+
+        public double Amount
+        {
+            get { return _amount; }
+        }
+    }
+}
