919c0690d2bd339757c9e98bab3e6bb82c7ccf74
Simon Jackson <simon@opengamma.com>
2011-07-28 16:00:53 +0100

    Finish commit: Check that we don't have old ignores in tests

---
+++ OGDotNet.Tests.Integration/OGDotNet/Resources/ViewTestsBaseTests.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ViewTestsBaseTests.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
+//     Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+//
+//     Please see distribution for license.
+// </copyright>
+//-----------------------------------------------------------------------
+using Xunit;
+
+namespace OGDotNet.Tests.Integration.OGDotNet.Resources
+{
+    public class ViewTestsBaseTests : ViewTestsBase
+    {
+        [Xunit.Extensions.Fact]
+        public void AllBannedViewsExist()
+        {
+            var definitionNames = Context.ViewProcessor.ViewDefinitionRepository.GetDefinitionNames();
+            foreach (var bannedView in BannedViews)
+            {
+                Assert.Contains(bannedView, definitionNames);
+            }
+        }
+    }
+}
