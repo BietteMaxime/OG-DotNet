45e168401af7d725ec3c8875f8d19a3f1af15a65
Simon Jackson <simon@opengamma.com>
2011-08-02 19:08:05 +0100

    Loosen timing assertion

--- OGDotNet.Tests.Integration/OGDotNet/Resources/MarketDataSnapshotProcessorTests.cs
+++ OGDotNet.Tests.Integration/OGDotNet/Resources/MarketDataSnapshotProcessorTests.cs
@@ -246,7 +246,7 @@ namespace OGDotNet.Tests.Integration.OGDotNet.Resources
             using (var dataSnapshotProcessor = snapshotManager.CreateFromViewDefinition(ViewDefinitionName))
             {
                 var fromStream = Time(() => dataSnapshotProcessor.PrepareUpdate());
-                Assert.InRange(fromStream, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+                Assert.InRange(fromStream, TimeSpan.Zero, TimeSpan.FromSeconds(2));
                 //TODO check that we have actually updated
             }
         }
