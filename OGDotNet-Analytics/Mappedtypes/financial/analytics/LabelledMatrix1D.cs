da16e173a61e98a0453fda342a9e7d9f6c3f5d32
Simon Jackson <simon@opengamma.com>
2011-08-08 12:55:04 +0100

    R#

--- OGDotNet-Analytics/Mappedtypes/financial/analytics/LabelledMatrix1D.cs
+++ OGDotNet-Analytics/Mappedtypes/financial/analytics/LabelledMatrix1D.cs
@@ -6,7 +6,6 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
