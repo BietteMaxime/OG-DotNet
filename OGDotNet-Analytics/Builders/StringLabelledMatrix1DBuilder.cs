aeee61554869e9642888cac3e8c62f10ca36f61a
Simon Jackson <simon@opengamma.com>
2011-08-08 12:54:55 +0100

    Handle StringLabelledMatrix1D

---
+++ OGDotNet-Analytics/Builders/StringLabelledMatrix1DBuilder.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="StringLabelledMatrix1DBuilder.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
+//     Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+//
+//     Please see distribution for license.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
+using Fudge;
+using OGDotNet.Mappedtypes.Financial.Analytics;
+
+namespace OGDotNet.Builders
+{
+    internal class StringLabelledMatrix1DBuilder : LabelledMatrix1DBuilder<string, StringLabelledMatrix1D>
+    {
+        public StringLabelledMatrix1DBuilder(FudgeContext context, Type type)
+            : base(context, type)
+        {
+        }
+
+        protected override string GetKey(IFudgeField field)
+        {
+            return (string)field.Value;
+        }
+    }
+}
---
+++ OGDotNet-Analytics/Mappedtypes/financial/analytics/StringLabelledMatrix1D.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="StringLabelledMatrix1D.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
+//     Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+//
+//     Please see distribution for license.
+// </copyright>
+//-----------------------------------------------------------------------
+using System.Collections.Generic;
+using System.Linq;
+using Fudge.Serialization;
+using OGDotNet.Builders;
+using OGDotNet.Utils;
+
+namespace OGDotNet.Mappedtypes.Financial.Analytics
+{
+    [FudgeSurrogate(typeof(StringLabelledMatrix1DBuilder))]
+    public class StringLabelledMatrix1D : LabelledMatrix1D<string>
+    {
+        public StringLabelledMatrix1D(IList<string> keys, IList<object> labels, IList<double> values) : base(keys, keys.Cast<object>().ToList(), values)
+        {
+            ArgumentChecker.Not(labels.Any(), "Didn't expect separate labels");
+        }
+    }
+}
