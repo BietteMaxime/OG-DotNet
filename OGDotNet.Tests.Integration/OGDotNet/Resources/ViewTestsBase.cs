106403375ceb97ef3980498d08690e41d9cfed8f
Simon Jackson <simon@opengamma.com>
2011-08-17 18:24:31 +0100

    Exclude another view

--- OGDotNet.Tests.Integration/OGDotNet/Resources/ViewTestsBase.cs
+++ OGDotNet.Tests.Integration/OGDotNet/Resources/ViewTestsBase.cs
@@ -33,6 +33,7 @@ namespace OGDotNet.Tests.Integration.OGDotNet.Resources
                                                                       "Multi-Currency Swap Test View (2)",
                                                                       "PoC Bond View",
                                                                       "PoC Bond View Implied",
+                                                                      "Equity Futures Test View",
                                                                       "Simple IR Future Option Test View" //PLAT-1459
                                                                   };
 
