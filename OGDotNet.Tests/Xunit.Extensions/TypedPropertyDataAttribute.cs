20cf2a4504fbc221ec172307e1d00d2dbf1a925d
Simon Jackson <simon@opengamma.com>
2011-08-18 13:14:40 +0100

    typed tuple tests

--- OGDotNet.Tests/Xunit.Extensions/TypedPropertyDataAttribute.cs
+++ OGDotNet.Tests/Xunit.Extensions/TypedPropertyDataAttribute.cs
@@ -11,6 +11,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using OGDotNet.Utils;
 using Xunit.Extensions;
 
 namespace OGDotNet.Tests.Xunit.Extensions
@@ -71,24 +72,69 @@ namespace OGDotNet.Tests.Xunit.Extensions
             var ienumInterface = interfaces.Where(i => i.FullName.StartsWith("System.Collections.Generic.IEnumerable`1")).ToList().FirstOrDefault();
             if (ienumInterface != null)
             {
+                var enumerableSource = ((IEnumerable)valueSource).Cast<object>();
                 var enumType = ienumInterface.GetGenericArguments().First();
-                switch (methodUnderTest.GetParameters().Length)
+                var paramsCount = methodUnderTest.GetParameters().Length;
+                switch (paramsCount)
                 {
                     case 1:
                         if (methodUnderTest.GetParameters()[0].ParameterType.IsAssignableFrom(enumType))
                         {
-                            return ((IEnumerable)valueSource).Cast<object>().Select(o => new[] { o });
+                            return enumerableSource.Select(o => new[] { o });
                         }
                         else
                         {
                             throw new ArgumentException(string.Format("I don't know how to pass {0} to parameter ({1})", enumType, methodUnderTest.GetParameters()[0]));
                         }
+                    case 3:
+                        if (! enumType.IsGenericType || enumType.GetGenericTypeDefinition() != typeof(Tuple<,,>))
+                        {
+                            throw new ArgumentException(string.Format("I don't know how to pass {0} to 3 parameters, use Tuple", enumType));
+                        }
+                        for (int i = 0; i < paramsCount; i++)
+                        {
+                            CheckTypeParameter(0, methodUnderTest, enumType);
+                        }
+                        var args = enumerableSource.Select(t => Enumerable.Range(0, 3).Select(i => GetTupleItem(t, i)).ToArray()).ToList();
+                        return args;
                     default:
                         throw new NotImplementedException("TODO: should probably handle tuples and/or property bags here");
                 }
             }
 
             throw new ArgumentException(string.Format("Property {0} on {1} did not return IEnumerable<object[]> or an IEnumerable<T>", _propertyName, declaringType.FullName));
+        }
+
+        private static object GetTupleItem(object tuple, int i)
+        {
+            var type = tuple.GetType();
+            MethodInfo getMethod = GetGetMethod(type, i);
+            return getMethod.Invoke(tuple, new object[] {});
+        }
+
+        static readonly Memoizer<Type, int, MethodInfo> GetGetMethodCache = new Memoizer<Type, int, MethodInfo>(GetGetMethodImpl);
+        private static MethodInfo GetGetMethod(Type type, int i)
+        {
+            return GetGetMethodCache.Get(type, i);
+        }
+        private static MethodInfo GetGetMethodImpl(Type type, int i)
+        {
+            var propertyInfo = type.GetProperty("Item" + (i + 1));
+            if (propertyInfo == null)
+            {
+                throw new ArgumentException(string.Format("Don't know how to get item {0} from {1}", i, type));
+            }
+            return propertyInfo.GetGetMethod();
+        }
+
+        private static void CheckTypeParameter(int index, MethodInfo methodUnderTest, Type enumType)
+        {
+            var paramtype = methodUnderTest.GetParameters()[index].ParameterType;
+            var tupleType = enumType.GetGenericArguments()[index];
+            if (tupleType != paramtype && paramtype.IsAssignableFrom(tupleType))
+            {
+                throw new ArgumentException(string.Format("I don't know how to pass {0} to parameter ({1})", tupleType, paramtype));
+            }
         }
     }
 }
