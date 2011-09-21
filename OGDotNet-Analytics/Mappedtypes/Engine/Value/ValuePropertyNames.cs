c1dbe07aec7a8dfc3cf0c95af6dec09f52f3cd23
Simon Jackson <simon@opengamma.com>
2011-09-21 14:10:45 +0100

    ValuePropertyNames

---
+++ OGDotNet-Analytics/Mappedtypes/Engine/Value/ValuePropertyNames.cs
@@ -0,0 +1,14 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ValuePropertyNames.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
+//     Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+//
+//     Please see distribution for license.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace OGDotNet.Mappedtypes.Engine.Value
+{
+    public static class ValuePropertyNames
+    {
+        public static readonly string Currency = "Currency";
+    }
+}
