69512d1bf15d7c27e242ca4ffa68cd9880b2e499
Simon Jackson <simon@opengamma.com>
2011-12-06 10:37:12 +0000

    Check that all services are present

--- OGDotNet.Tests.Integration/OGDotNet/Model/Context/RemoteEngineContextTests.cs
+++ OGDotNet.Tests.Integration/OGDotNet/Model/Context/RemoteEngineContextTests.cs
@@ -7,6 +7,7 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Linq;
 using OGDotNet.Model.Context;
 using OGDotNet.Tests.Integration.Properties;
 using Xunit;
@@ -26,6 +27,33 @@ namespace OGDotNet.Tests.Integration.OGDotNet.Model.Context
         public void RootUriAsExpected()
         {
             Assert.Equal(new Uri(Settings.Default.ServiceUri), GetContext().RootUri);
+        }
+
+        [Fact]
+        public void CanGetAllServices()
+        {
+            var context = GetContext();
+            var type = context.GetType();
+            foreach (var prop in type.GetProperties())
+            {
+                var service = prop.GetGetMethod().Invoke(context, new object[] { });
+                if (service == null)
+                {
+                    throw new Exception("Null service " + prop.Name);
+                }
+            }
+            foreach (var method in type.GetMethods().Where(m => !m.IsSpecialName && m.GetParameters().Count() == 0 && m.DeclaringType != typeof(object)))
+            {
+                var result = method.Invoke(context, new object[] {});
+                if (result == null)
+                {
+                    throw new Exception("Null service " + method.Name);
+                }
+                if (result is IDisposable)
+                {
+                    ((IDisposable) result).Dispose();
+                }
+            }
         }
 
         internal static RemoteEngineContext GetContext()
