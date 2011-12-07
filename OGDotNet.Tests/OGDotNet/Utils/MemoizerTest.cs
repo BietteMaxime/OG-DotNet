5d78b80df773f4c24462eb847e6cb87a1cbf4e5f
Simon Jackson <simon@opengamma.com>
2011-12-07 18:07:46 +0000

    Add test to document the subtle behaviour of Memoizer

---
+++ OGDotNet.Tests/OGDotNet/Utils/MemoizerTest.cs
@@ -0,0 +1,55 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MemoizerTest.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
+//     Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+//
+//     Please see distribution for license.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using OGDotNet.Utils;
+using Xunit;
+
+namespace OGDotNet.Tests.OGDotNet.Utils
+{
+    public class MemoizerTest
+    {
+        [Fact]
+        public void ConcurrentAlwaysAddsReturnSingelton()
+        {
+            const int repeats = 10;
+            for (int i = 0; i < repeats; i++)
+            {
+                ConcurrentAddsReturnSingelton();
+            }
+            Parallel.For(1, repeats, i => ConcurrentAddsReturnSingelton());
+        }
+
+        [Fact]
+        public void ConcurrentAddsReturnSingelton()
+        {
+            var wait = TimeSpan.FromMinutes(1);
+
+            var b = new Barrier(2);
+            long functionCount = 0;
+            var m = new Memoizer<int, object>(i =>
+                                          {
+                                              var count = Interlocked.Increment(ref functionCount);
+                                              if (count > 2)
+                                              {
+                                                  throw new Exception("Too many executions " + count);
+                                              }
+                                              Assert.True(b.SignalAndWait(wait));
+                                              return new object();
+                                          });
+            const int arg = 1;
+            var o1 = Task<object>.Factory.StartNew(() => m.Get(arg));
+            var o2 = Task<object>.Factory.StartNew(() => m.Get(arg));
+            Assert.True(o1.Wait(wait));
+            Assert.True(o2.Wait(wait));
+            Assert.Equal(2, functionCount);
+            Assert.Equal(o1.Result, o2.Result);
+        }
+    }
+}
