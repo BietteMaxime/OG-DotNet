40627e1641a7c7ffe3e056ed192109b712609a9b
Simon Jackson <simon@opengamma.com>
2011-12-15 13:55:03 +0000

    Log to LocalData, and from both demo applications

---
+++ OGDotNet-Analytics/log4net/SpecialFolderPatternConverter.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SpecialFolderPatternConverter.cs" company="OpenGamma Inc. and the OpenGamma group of companies">
+//     Copyright © 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+//
+//     Please see distribution for license.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
+
+namespace OGDotNet.log4net
+{
+    public class SpecialFolderPatternConverter : global::log4net.Util.PatternConverter
+    {
+        protected override void Convert(System.IO.TextWriter writer, object state)
+        {
+            var specialFolder = (Environment.SpecialFolder)Enum.Parse(typeof(Environment.SpecialFolder), Option, true);
+
+            writer.Write(Environment.GetFolderPath(specialFolder));
+        }
+    }
+}
